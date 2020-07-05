4df840bd335b8f265788decfdc231754845b91b2
krisharma <krishsharma1128@gmail.com>
2020-07-04 21:29:24 -0400

    create react app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './sandbox_square_salmon.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Sandbox A-Eye Project Landing Page
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
