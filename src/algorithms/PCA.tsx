1db6f96aec0ba14cf4823040ff3db8fd27eaad34
Iris Liu <Iris.Lamb2@gmail.com>
2020-09-06 16:41:42 -0400

    Create initial PCA graph

--- src/algorithms/PCA.tsx
+++ src/algorithms/PCA.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { PCA } from 'ml-pca';
 import datasetIris from 'ml-dataset-iris';
+import { Scatter } from 'react-chartjs-2';
+// you can also directly use chartjs without the wrapper for more customization
 
 /**
  * Interactive demo of PCA.
@@ -16,19 +18,15 @@ const PCADemo = ({ }) => {
     // deployment for ts? installs? package managment in general
     // we should have text explanations for each step
 
-    const dataset: number[][] = datasetIris.getNumbers(); // rows represent the samples and columns the features
-    const pca = new PCA(dataset);
-    const prediction = pca.predict(dataset); //plot columns 1, 2
-
-    // todo pls refactor some of this shit
     return (
         <div className="PCA-div">
-            <RawDataTable dataset={dataset}/>
+            {/* <RawDataTable dataset={dataset} /> */}
+            <PCAChart points={pcPoints} />
         </div>
     );
 }
 
-const RawDataTable = ({dataset}: {dataset: number[][]}) =>
+const RawDataTable = ({ dataset }: { dataset: number[][] }) =>
     <table className="raw-data">
         <tr><th>Sepal Length</th><th>Sepal Width</th><th>Petal Length</th><th>Petal Width</th></tr>
         {dataset.map((row: number[], idx: number) => {
@@ -42,10 +40,42 @@ const RawDataTable = ({dataset}: {dataset: number[][]}) =>
 
 // Plot all samples in dataset, choose what 2 features to use as the axes
 // --> natural conclusion: want to be able to see all features, without having to use an nth dimensional plot
-const rawDataChart = ({dataset, feature1, feature2}: {dataset: number[][], feature1: number, feature2: number}) => {}; 
+const RawDataChart = ({ dataset, feature1, feature2 }: { dataset: number[][], feature1: number, feature2: number }) => { };
 
-// plot all features using PC1, PC2 as axes --> easy to see what the flower species are as clusters!!
-// where do i get the pc1, pc2 values for points in a dataset.
+const PCAChart = ({ points }: { points: Array<{ x: number, y: number }> }) => {
+    const data = {
+        labels: ['Scatter'],
+        datasets: [{
+            label: 'Flower Sample',
+            fill: true,
+            pointRadius: 4,
+            backgroundColor: '#605196',
+            data: points
+        }]
+    };
+    const options = {
+        showLines: false,
+        tooltips: { enabled: false },
+        scales: {
+            yAxes: [{
+                scaleLabel: { display: true, labelString: 'PC2' }
+            }],
+            xAxes: [{
+                scaleLabel: { display: true, labelString: 'PC1' }
+            }],
+        }
+    };
+    return (<Scatter data={data} options={options} />);
+}
+
+// Moving these outside so they are only calculated once (this will change if we dynamically get datasets)
+const dataset: number[][] = datasetIris.getNumbers(); // rows represent the samples and columns the features
+const pca = new PCA(dataset);
+const prediction = pca.predict(dataset); //plot columns 1, 2
+const pcPoints: Array<{ x: number, y: number }> = [];
+for (let i = 0; i < prediction.rows; i++) {
+    pcPoints.push({ x: prediction.get(i, 0), y: prediction.get(i, 1) });
+}
 
 
 export default PCADemo;
