34e787984282b83e0f26326c11d29fe1a20e92d6
Iris Liu <Iris.Lamb2@gmail.com>
2020-09-06 15:06:54 -0400

    get rid of irrelevant comments

--- src/algorithms/PCA.tsx
+++ src/algorithms/PCA.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import { PCA } from 'ml-pca';
 import datasetIris from 'ml-dataset-iris';
@@ -15,31 +14,11 @@ const PCADemo = ({ }) => {
     // yarn vs npm?
     // material ui?
     // deployment for ts? installs? package managment in general
-    // what to use for graphs? echarts?
-    // dunno what to use to plot the pca stuff - which functions? what numbers am i plotting??
     // we should have text explanations for each step
 
     const dataset: number[][] = datasetIris.getNumbers(); // rows represent the samples and columns the features
     const pca = new PCA(dataset);
-    console.log(pca.getExplainedVariance());
-    /*
-    [ 0.9246187232017269,
-      0.05306648311706785,
-      0.017102609807929704,
-      0.005212183873275558 ]
-    */
-    const newPoints = [[4.9, 3.2, 1.2, 0.4], [5.4, 3.3, 1.4, 0.9]];
-    console.log(pca.predict(newPoints));
-    /*
-    [[ -2.830722471866897,
-        0.01139060953209596,
-        0.0030369648815961603,
-        -0.2817812120420965 ],
-    [ -2.308002707614927,
-        -0.3175048770719249,
-        0.059976053412802766,
-        -0.688413413360567 ]]
-    */
+    const prediction = pca.predict(dataset); //plot columns 1, 2
 
     // todo pls refactor some of this shit
     return (
