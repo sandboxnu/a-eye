9428905d320ef0d69946d2718efc2931a8bad270
Max Pinheiro <maxpinheiro181@gmail.com>
2020-09-16 21:02:14 -0400

    Add navbar to landing page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
+import Navbar from "./Navbar";
 import logo from './sandbox_square_salmon.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App bg-red-500">
-	<button className="bg-teal-400">Button Name</button>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Sandbox A-Eye Project Landing Page
-        </p>
-      </header>
-    </div>
+      <div className="App">
+          <div className="App-header">
+              <Navbar />
+          </div>
+
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+              Sandbox A-Eye Project Landing Page
+          </p>
+      </div>
   );
 }
 
