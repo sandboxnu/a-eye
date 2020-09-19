4a3fa9f42aac7f18e8f94fbd63f6f40e5e122fe8
Iris Liu <Iris.Lamb2@gmail.com>
2020-09-19 17:28:17 -0400

    Add actual photos and links

--- src/aboutPage/AboutPage.tsx
+++ src/aboutPage/AboutPage.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ProfileCard from './ProfileCard';
-import kevin from './profileAssets/kevin.jpg';
+import krishPic from './profileAssets/krishPic.png';
+import liliPic from './profileAssets/liliPic.png';
+import cassiePic from './profileAssets/cassiePic.png';
+import danPic from './profileAssets/danPic.jpg';
+import irisPic from './profileAssets/irisPic.jpg';
+import maxPic from './profileAssets/maxPic.png';
 
 const AboutPage = () => {
-    const KevinProfile = () =>
-        <ProfileCard image={kevin} name="Kevin" title="Lord Eternal"
-            linkedin="https://www.linkedin.com/in/iris-liu-curiously/"
-            email="liu.i@northeastern.edu" />
     return (
         <div className="absolute w-full min-h-full bg-offwhite bg-about bg-no-repeat bg-scroll bg-bottom bg-stretchBottom">
             <div>
@@ -18,7 +19,24 @@ const AboutPage = () => {
                 </h2>
             </div>
             <div id="profile-cards-section" className="m-auto mt-32 w-3/4 flex flex-wrap justify-center">
-                <KevinProfile/> <KevinProfile/> <KevinProfile/> <KevinProfile/> <KevinProfile/> <KevinProfile/> <KevinProfile/>
+                <ProfileCard image={krishPic} name="Krish Sharma" title="Project Lead"
+                    linkedin="https://www.linkedin.com/in/krish-sharma/"
+                    email="sharma.kri@northeastern.edu" />
+                <ProfileCard image={liliPic} name="Lili Kobayashi" title="Designer"
+                    linkedin="http://linkedin.com/in/lili-kobayashi"
+                    email="Kobayashi.l@northeastern.edu" />
+                <ProfileCard image={cassiePic} name="Cassie Harbour" title="Developer"
+                    linkedin=""
+                    email="harbour.c@northeastern.edu" />
+                <ProfileCard image={danPic} name="Dan Krasnonosenkikh" title="Developer"
+                    linkedin="https://www.linkedin.com/in/dankrasno/"
+                    email="krasnonosenkikh.d@northeastern.edu" />
+                <ProfileCard image={irisPic} name="Iris Liu" title="Developer"
+                    linkedin="https://www.linkedin.com/in/iris-liu-curiously"
+                    email="liu.i@northeastern.edu" />
+                <ProfileCard image={maxPic} name="Max Pinheiro" title="Developer"
+                    linkedin="https://www.linkedin.com/in/max-pinheiro-4a5b11181/"
+                    email="pinheiro.m@northeastern.edu" />
             </div>
         </div>
     );
