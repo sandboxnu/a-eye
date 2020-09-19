ac4afb59228b63383bb6db26f72ef5f2cd1cca05
Max Pinheiro <maxpinheiro181@gmail.com>
2020-09-18 21:05:34 -0400

    add responsive design to navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,32 +3,20 @@ import logo from './sandbox_logo.png';
 
 export default function Navbar() {
     return (
-        <nav id="navbar-container" className="flex justify-between bg-navbar px-2 font-opensans font-bold">
-            <div id="logo-container" className="flex px-40 py-1">
-                <img src={logo} className="h-10" alt="logo" />
+        <nav className="flex justify-between px-2 bg-navbar" id="navbar-container">
+            <div className="flex my-1 ml-8 sm:ml-12 lg:ml-40" id="logo-container">
+                <img src={logo} className="w-24 h-10" alt="logo" />
             </div>
-            <div id="links-container" className="py-3 justify-between list-none">
-                <ul id="nav-list" className="flex px-40">
-                    <li id="nav-item" className="mx-8 border-b-2 border-transparent hover:border-teal-300 duration-300 ease-in-out">
-                        <a className="" href="/home">
-                            <p className="text-xs text-white uppercase">
-                                Home
-                            </p>
-                        </a>
+            <div className="my-3 justify-between" id="links-container">
+                <ul className="flex sm:mr-12 lg:mr-40" id="nav-list">
+                    <li className="mr-8 border-b-2 border-transparent hover:border-teal-300 duration-300 ease-in-out">
+                        <a className="" href="/home"><p className="text-xs text-white uppercase font-opensans font-bold">Home</p></a>
                     </li>
-                    <li id="nav-item" className="mx-8 border-b-2 border-transparent hover:border-teal-300 duration-300 ease-in-out">
-                        <a className="" href="/modules">
-                            <p className="text-xs text-white uppercase">
-                                Modules
-                            </p>
-                        </a>
+                    <li className="mx-8 border-b-2 border-transparent hover:border-teal-300 duration-300 ease-in-out">
+                        <a className="" href="/modules"><p className="text-xs text-white uppercase font-opensans font-bold">Modules</p></a>
                     </li>
-                    <li id="nav-item" className="mx-8 border-b-2 border-transparent hover:border-teal-300 duration-300 ease-in-out">
-                        <a className="" href="/about">
-                            <p className="text-xs text-white uppercase">
-                                About
-                            </p>
-                        </a>
+                    <li className="mx-8 border-b-2 border-transparent hover:border-teal-300 duration-300 ease-in-out">
+                        <a className="" href="/about"><p className="text-xs text-white uppercase font-opensans font-bold">About</p></a>
                     </li>
                 </ul>
             </div>
