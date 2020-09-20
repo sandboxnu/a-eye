74f3bff2ce6d2cb9dbe5f407b59649b0cbe067e4
harbourcd <51460895+harbourcd@users.noreply.github.com>
2020-09-20 16:38:08 -0400

    hover added

--- src/footer.js
+++ src/footer.js
@@ -27,7 +27,7 @@ function Footer() {
                 </div>
 
             </div>
-            <div className="flex bg-scroll w-3 sm:w-6 lg:w-20  justify-center" id="scroll-top-container" onClick={scrollTop}>
+            <div className="flex bg-scroll w-3 sm:w-6 lg:w-20  justify-center  hover:text-teal-300 duration-300 ease-in-out" id="scroll-top-container" onClick={scrollTop}>
                  <svg class="w-6 h-6 m-auto" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 10l7-7m0 0l7 7m-7-7v18"></path>
                 </svg>
