13822ac14d3b8295261a86f587f1e5f94e012f20
Iris Liu <Iris.Lamb2@gmail.com>
2020-09-27 15:52:35 -0400

    Finish filtering functionality

--- src/gaussianBlur/GaussianBlurDemo.tsx
+++ src/gaussianBlur/GaussianBlurDemo.tsx
@@ -1,6 +1,8 @@
 import React, { useLayoutEffect, useRef } from 'react';
-import { filterImage, convolute } from './filter';
+import { convolute } from './filter';
 import politeCat from './politeCat.png';
+import bebby from './bebby.png';
+import jellyfish from './jellyfish.png';
 
 // http://dev.theomader.com/gaussian-kernel-calculator/
 // https://blog.cloudboost.io/using-html5-canvas-with-react-ff7d93f5dc76
@@ -18,19 +20,30 @@ const GaussianBlurDemo = () => {
 
         imgElem.onload = () => {
             inputElem.getContext('2d')?.drawImage(imgElem, 0, 0);
-            filterImage(convolute, inputElem, outputElem, imgElem,
-                [.07, .1, .07,
-                .1, .2, .1,
-                .07, .1, .07]);
+            outputElem.getContext('2d')?.drawImage(imgElem, 0, 0);
         }
     }, [])
 
+    const applyFilter = () => {
+        const inputElem = inputCanvas.current; // get the DOM element for the canvas
+        const outputElem = outputCanvas.current;
+        if (!(inputElem && outputElem)) return;
+        convolute(inputElem, outputElem, true, 
+            [
+                0.036894,	0.039167,	0.039956,	0.039167,	0.036894,
+                0.039167,	0.041581,	0.042418,	0.041581,	0.039167,
+                0.039956,	0.042418,	0.043272,	0.042418,	0.039956,
+                0.039167,	0.041581,	0.042418,	0.041581,	0.039167,
+                0.036894,	0.039167,	0.039956,	0.039167,	0.036894]);
+    }
+
     // hardcoded width and height for canvas
     return (
-        <div>
-            <canvas ref={inputCanvas} width={640} height={640} />
-            <canvas ref={outputCanvas} width={640} height={640} />
-            <img ref={imgRef} src={politeCat} alt="input" className='hidden' />
+        <div className="inline">
+            <canvas className="inline" ref={inputCanvas} width={640} height={640} />
+            <button onClick={applyFilter}> Apply Filter </button>
+            <canvas className="inline" ref={outputCanvas} width={640} height={640} />
+            <img ref={imgRef} src={jellyfish} alt="input" className='hidden' />
         </div>
     )
 }
--- src/gaussianBlur/filter.tsx
+++ src/gaussianBlur/filter.tsx
@@ -1,90 +1,95 @@
 
 export {
-    filterImage,
-    convolute
+  convolute
 };
 
-/// utility functions for filtering / manipulating a DOM canvas?
+/// utility functions for filtering / manipulating a DOM canvas
 // reference: https://www.html5rocks.com/en/tutorials/canvas/imagefilters/
 
 /**
- * Filters the image on the input canvas and draws it to the output. 
- * 
- * @param filter function for filtering an image
- * @param inCanvas the canvas to be filtered, with an image already drawn on it
- * @param outCanvas the empty canvas to be outputted to
- */
-function filterImage(filter : FilterFunction, inCanvas : HTMLCanvasElement, 
-    outCanvas : HTMLCanvasElement, ...args: any[]) {
-    const pixels = getPixels(inCanvas);
-    const output = createImageData(outCanvas);
-    const newImgData =  pixels && output && filter(pixels, output, ...args);
-    if (newImgData) outCanvas.getContext("2d")?.putImageData(newImgData, 0, 0);
-}
-
-function getPixels(canvas : HTMLCanvasElement) {
-    var ctx = canvas.getContext('2d');
-    return ctx && ctx.getImageData(0,0,canvas.width,canvas.height);
-}
-
-function createImageData(canvas : HTMLCanvasElement) {
-    var ctx = canvas.getContext('2d');
-    return ctx && ctx.createImageData(canvas.width,canvas.height);
-}
-
-///// Filter functions below
-
-/**
- * Run an image convolution.
+ * Run an image convolution based on inCanvas, and draws the output to outCanvas.
  * Don't ask me how any of this works!!!! I dont know!!!!!!! 
  * 
- * @param pixels input image data
- * @param output image data to be written to
+ * @param inCanvas the canvas to be filtered, with an image already drawn on it
+ * @param outCanvas the canvas to be outputted to
+ * @param animate whether or not to animate the filter by each row of pixels
  * @param weights the kernel (matrix of numbers)
  * @param opaque (optional) is the image opaque?
  */
-function convolute(pixels : ImageData, output : ImageData, weights : number[], opaque? : boolean) {
-    var side = Math.round(Math.sqrt(weights.length));
-    var halfSide = Math.floor(side/2);
-    var src = pixels.data;
-    var sw = pixels.width;
-    var sh = pixels.height;
-    // pad output by the convolution matrix
-    var w = sw;
-    var h = sh;
-    var dst = output.data;
-    // go through the destination image pixels
-    var alphaFac = opaque ? 1 : 0;
-    for (var y=0; y<h; y++) {
-      for (var x=0; x<w; x++) {
-        var sy = y;
-        var sx = x;
-        var dstOff = (y*w+x)*4;
-        // calculate the weighed sum of the source image pixels that
-        // fall under the convolution matrix
-        var r=0, g=0, b=0, a=0;
-        for (var cy=0; cy<side; cy++) {
-          for (var cx=0; cx<side; cx++) {
-            var scy = sy + cy - halfSide;
-            var scx = sx + cx - halfSide;
-            if (scy >= 0 && scy < sh && scx >= 0 && scx < sw) {
-              var srcOff = (scy*sw+scx)*4;
-              var wt = weights[cy*side+cx];
-              r += src[srcOff] * wt;
-              g += src[srcOff+1] * wt;
-              b += src[srcOff+2] * wt;
-              a += src[srcOff+3] * wt;
-            }
+function convolute(inCanvas: HTMLCanvasElement, outCanvas: HTMLCanvasElement, animate: boolean, weights: number[], opaque?: boolean) {
+  const pixels = getPixels(inCanvas);
+  const output = getPixels(outCanvas);
+  if (!pixels || !output) return;
+
+  var side = Math.round(Math.sqrt(weights.length));
+  var halfSide = Math.floor(side / 2);
+  var src = pixels.data;
+  var sw = pixels.width;
+  var sh = pixels.height;
+  // pad output by the convolution matrix
+  var w = sw;
+  var h = sh;
+  var dst = output.data;
+  // go through the destination image pixels
+  var alphaFac = opaque ? 1 : 0;
+
+  const processRow = (y : number) => {
+    for (var x = 0; x < w; x++) {
+      var sy = y;
+      var sx = x;
+      var dstOff = (y * w + x) * 4;
+      var nextDstOff = ((y + 1) * w + x) * 4;
+      // calculate the weighed sum of the source image pixels that
+      // fall under the convolution matrix
+      var r = 0, g = 0, b = 0, a = 0;
+      for (var cy = 0; cy < side; cy++) {
+        for (var cx = 0; cx < side; cx++) {
+          var scy = sy + cy - halfSide;
+          var scx = sx + cx - halfSide;
+          if (scy >= 0 && scy < sh && scx >= 0 && scx < sw) {
+            var srcOff = (scy * sw + scx) * 4;
+            var wt = weights[cy * side + cx];
+            r += src[srcOff] * wt;
+            g += src[srcOff + 1] * wt;
+            b += src[srcOff + 2] * wt;
+            a += src[srcOff + 3] * wt;
           }
         }
-        dst[dstOff] = r;
-        dst[dstOff+1] = g;
-        dst[dstOff+2] = b;
-        dst[dstOff+3] = a + alphaFac*(255-a);
       }
+      dst[dstOff] = r;
+      dst[dstOff + 1] = g;
+      dst[dstOff + 2] = b;
+      dst[dstOff + 3] = a + alphaFac * (255 - a);
+      dst[nextDstOff] = 255;
+      dst[nextDstOff + 1] = 0;
+      dst[nextDstOff + 2] = 0;
     }
-    return output;
+  }
+
+  if (animate) {
+    var y = 0;
+    const interval = setInterval(() => {
+      processRow(y);
+      outCanvas.getContext("2d")?.putImageData(output, 0, 0);
+      y++;
+      if (y >= h) clearInterval(interval);
+    }, 10);
+  } else {
+    for (var y = 0; y < h; y++) {
+      processRow(y);
+    }
+    outCanvas.getContext("2d")?.putImageData(output, 0, 0);
+  }
 }
 
-type FilterFunction = (pixels : ImageData, output : ImageData, ...args: any[]) => ImageData;
+
+function getPixels(canvas: HTMLCanvasElement) {
+  var ctx = canvas.getContext('2d');
+  return ctx && ctx.getImageData(0, 0, canvas.width, canvas.height);
+}
+
+function createImageData(canvas: HTMLCanvasElement) {
+  var ctx = canvas.getContext('2d');
+  return ctx && ctx.createImageData(canvas.width, canvas.height);
+}
 
