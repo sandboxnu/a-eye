5973fe5a641f4ffd4d415b662fe4532410c4da7e
Iris Liu <Iris.Lamb2@gmail.com>
2020-10-01 21:14:34 -0400

    Create UI for configuring Gaussian

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Navbar from "./Navbar";
 import Footer from './footer';
 import './App.css';
-import AboutPage from "./aboutPage/AboutPage"
+import AboutPage from "./aboutPage/AboutPage";
+import GaussianBlurDemo from './gaussianBlur/GaussianBlurDemo';
 
 function App() {
   return (
@@ -12,9 +13,10 @@ function App() {
         <div className="App-header">
           <Navbar />
         </div>
-        <main className="font-mono text-lg">
+        <main className="font-mono text-lg font-charcoal">
           <Switch>
             <Route path="/about" component={AboutPage} />
+            <Route path="/home" component={GaussianBlurDemo} />
           </Switch>
         </main>
         <div>
--- src/gaussianBlur/GaussianBlurDemo.tsx
+++ src/gaussianBlur/GaussianBlurDemo.tsx
@@ -1,51 +1,84 @@
-import React, { useLayoutEffect, useRef } from 'react';
-import { convolute } from './filter';
-import politeCat from './politeCat.png';
-import bebby from './bebby.png';
+import React, { ChangeEvent, useLayoutEffect, useRef, useState } from 'react';
 import jellyfish from './jellyfish.png';
-
-// http://dev.theomader.com/gaussian-kernel-calculator/
-// https://blog.cloudboost.io/using-html5-canvas-with-react-ff7d93f5dc76
+import InteractiveFilter from './InteractiveFilter';
 
 const GaussianBlurDemo = () => {
-    const inputCanvas = useRef<HTMLCanvasElement>(null);
-    const outputCanvas = useRef<HTMLCanvasElement>(null);
-    const imgRef = useRef<HTMLImageElement>(null);
+    const [kernelSize, setKernelSize] = useState(5);
+    const [sigma, setSigma] = useState(1);
 
-    useLayoutEffect(() => {
-        const inputElem = inputCanvas.current; // get the DOM element for the canvas
-        const outputElem = outputCanvas.current;
-        const imgElem = imgRef.current;
-        if (!(inputElem && outputElem && imgElem)) return;
+    const kernel =
+        // [[0.077847,	0.123317,	0.077847],
+        //     [0.123317,	0.195346,	0.123317],
+        //     [0.077847,	0.123317,	0.077847]]
+        [[0.036894, 0.039167, 0.039956, 0.039167, 0.036894],
+        [0.039167, 0.041581, 0.042418, 0.041581, 0.039167],
+        [0.039956, 0.042418, 0.043272, 0.042418, 0.039956],
+        [0.039167, 0.041581, 0.042418, 0.041581, 0.039167],
+        [0.036894, 0.039167, 0.039956, 0.039167, 0.036894]];
+    const max = kernel[Math.floor(kernelSize / 2)][Math.floor((kernelSize / 2))];
+    const min = kernel[0][0];
 
-        imgElem.onload = () => {
-            inputElem.getContext('2d')?.drawImage(imgElem, 0, 0);
-            outputElem.getContext('2d')?.drawImage(imgElem, 0, 0);
-        }
-    }, [])
 
-    const applyFilter = () => {
-        const inputElem = inputCanvas.current; // get the DOM element for the canvas
-        const outputElem = outputCanvas.current;
-        if (!(inputElem && outputElem)) return;
-        convolute(inputElem, outputElem, true, 
-            [
-                0.036894,	0.039167,	0.039956,	0.039167,	0.036894,
-                0.039167,	0.041581,	0.042418,	0.041581,	0.039167,
-                0.039956,	0.042418,	0.043272,	0.042418,	0.039956,
-                0.039167,	0.041581,	0.042418,	0.041581,	0.039167,
-                0.036894,	0.039167,	0.039956,	0.039167,	0.036894]);
-    }
 
-    // hardcoded width and height for canvas
     return (
-        <div className="inline">
-            <canvas className="inline" ref={inputCanvas} width={640} height={640} />
-            <button onClick={applyFilter}> Apply Filter </button>
-            <canvas className="inline" ref={outputCanvas} width={640} height={640} />
-            <img ref={imgRef} src={jellyfish} alt="input" className='hidden' />
+        <div className="m-4">
+            <KernelConfig initSigma={sigma} initSize={kernelSize} />
+            <div className="m-4">
+                Gaussian Kernel
+                <table className="m-auto"><tbody>
+                    {kernel.map((row, i) => (
+                        <tr key={i}>
+                            {row.map((val, j) => (
+                                <td key={j}
+                                    style={getBg(val, max, min)}
+                                    className="border-2 border-charcoal p-2"
+                                    title={`${val}`}>
+                                    {val}
+                                </td>))}
+                        </tr>
+                    ))}
+                </tbody></table>
+            </div>
+            <InteractiveFilter kernel={kernel.reduce((list, row) => list.concat(row), [])} imgUrl={jellyfish} />
         </div>
     )
+}
+
+const KernelConfig = (props: { initSize: number, initSigma: number }) => {
+    const [kernelSize, setKernelSize] = useState(props.initSize);
+    const [sigma, setSigma] = useState(props.initSigma);
+
+    const changeSigma = (e: any) => setSigma(e.target.value);
+    const changeKernelSize = (e: any) => setKernelSize(e.target.value);
+
+    return (
+        <div>
+            <div className="font-bold m-3">
+                Sigma
+                <input className="mx-2 w-64"
+                    type="range" min=".1" max="10"
+                    value={sigma} onChange={(e) => changeSigma(e)} />
+                <input className="number-input"
+                    type="number" min=".1" max="10"
+                    value={sigma} onChange={(e) => changeSigma(e)} />
+            </div>
+            <div className="font-bold m-3">
+                Kernel Size
+                <input className="mx-2 w-64"
+                    type="range" min="1" max="105" step={2}
+                    value={kernelSize} onChange={(e) => changeKernelSize(e)} />
+                <input className="number-input"
+                    type="number" min="1" max="105" step={2}
+                    value={kernelSize} onChange={(e) => changeKernelSize(e)} />
+            </div>
+            <button className="basic-button">Apply</button>
+        </div>
+    );
+}
+
+function getBg(val: number, max: number, min: number) {
+    const red = 200 - ((val - min) / (max - min) * 200);
+    return { background: `rgb(${red}, 212, 192)` };
 }
 
 export default GaussianBlurDemo;
