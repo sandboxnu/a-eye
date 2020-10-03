7ce60ae758ccb049d91d988e79920005a9985b89
Max Pinheiro <maxpinheiro181@gmail.com>
2020-10-03 11:44:01 -0400

    replace template with real module info

--- src/landingPage/LandingPage.js
+++ src/landingPage/LandingPage.js
@@ -1,32 +1,27 @@
 import React from 'react';
 import background_img from '../media/background.jpeg';
 import landing_background from '../media/background_landing_page.svg';
+import descriptions from '../media/module_descriptions.json';
 import ModuleIntro from "./ModuleIntro";
 
 let top_style = {backgroundImage: `url(${background_img})`};
-
 let bot_style = {backgroundImage: `url(${landing_background})`};
-
-let lorem = 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod';
 
 export default function LandingPage() {
   return (
       <div className="container w-screen">
-        <div className="bg-fixed bg-cover w-screen h-screen" style={top_style}></div>
+        <div className="bg-fixed bg-cover w-screen h-screen" style={top_style}>
+
+        </div>
         <div className="flex items-center bg-cover w-screen h-auto" style={bot_style}>
             <ul className="list-none">
-                <li className="flex justify-center my-10">
-                    <ModuleIntro title="Week 1: State Spaces" body={lorem} color="bg-lightblue"/>
-                </li>
-                <li className="flex justify-center my-10">
-                    <ModuleIntro title="Week 1: State Spaces" body={lorem} color="bg-darkblue"/>
-                </li>
-                <li className="flex justify-center my-10">
-                    <ModuleIntro title="Week 1: State Spaces" body={lorem} color="bg-offwhite"/>
-                </li>
-                <li className="flex justify-center my-10">
-                    <ModuleIntro title="Week 1: State Spaces" body={lorem} color="bg-darkblue"/>
-                </li>
+                {
+                    descriptions.modules.map((module) =>
+                        <li className="flex justify-center my-10">
+                            <ModuleIntro title={module.title} body={module.body} bgColor={module.bgColor} textColor={module.textColor} />
+                        </li>
+                    )
+                }
             </ul>
         </div>
       </div>
--- src/landingPage/ModuleIntro.js
+++ src/landingPage/ModuleIntro.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function ModuleIntro(props) {
     return (
-        <div className={"flex-col w-1/2 rounded-lg " + props.color}>
+        <div className={`flex-col w-1/2 rounded-lg ${props.bgColor} ${props.textColor}`}>
             <h1 className="text-xl font-opensans">{props.title}</h1>
             <div className="flex-row inline-flex justify-between m-8">
                 <img className="w-1/5" alt="img"/>
