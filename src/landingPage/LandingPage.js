eb419a8ae34079993970ebd22d6c506c4c04d39a
Max Pinheiro <maxpinheiro181@gmail.com>
2020-10-03 13:46:37 -0400

    main screen features

--- src/landingPage/LandingPage.js
+++ src/landingPage/LandingPage.js
@@ -10,8 +10,14 @@ let bot_style = {backgroundImage: `url(${landing_background})`};
 export default function LandingPage() {
   return (
       <div className="container w-screen">
-        <div className="bg-fixed bg-cover w-screen h-screen" style={top_style}>
-
+        <div className="flex justify-center items-center bg-fixed bg-cover w-screen h-screen" style={top_style}>
+            <div className="mb-16">
+                <p className="text-center text-white text-5xl italic font-roboto my-4">Your Eye and AI</p>
+                <p className="text-center text-white text-xl font-roboto my-4">Professor Mingolla</p>
+            </div>
+            <div className="absolute bottom-0 mb-20">
+                <p className="text-white text-md font-roboto">Developed by Sandbox</p>
+            </div>
         </div>
         <div className="flex items-center bg-cover w-screen h-auto" style={bot_style}>
             <ul className="list-none">
--- src/landingPage/ModuleIntro.js
+++ src/landingPage/ModuleIntro.js
@@ -5,8 +5,8 @@ export default function ModuleIntro(props) {
         <div className={`flex-col w-1/2 rounded-lg ${props.bgColor} ${props.textColor}`}>
             <h1 className="text-xl font-opensans">{props.title}</h1>
             <div className="flex-row inline-flex justify-between m-8">
-                <img className="w-1/5" alt="img"/>
-                <p className="w-3/5 text-sm font-roboto my-4">{props.body}</p>
+                <img className="w-1/3" alt="img"/>
+                <p className="w-2/3 text-sm font-roboto my-4">{props.body}</p>
             </div>
         </div>
     );
