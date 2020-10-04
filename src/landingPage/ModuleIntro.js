61a99b89f719274d8fb2fbeda58adf3d33241d9d
Max Pinheiro <maxpinheiro181@gmail.com>
2020-10-04 18:04:36 -0400

    larger description text size

--- src/landingPage/ModuleIntro.js
+++ src/landingPage/ModuleIntro.js
@@ -6,7 +6,7 @@ export default function ModuleIntro(props) {
             <h1 className="text-3.5xl float-left italic font-bold font-opensans mt-5 ml-10">{props.title}</h1>
             <div className="flex-row inline-flex justify-between m-8 mt-4">
                 <img src={process.env.PUBLIC_URL + props.imgSrc} className="w-1/4 ml-4 object-contain" alt="img"/>
-                <p className="w-2/3 text-sm font-mono my-4 text-left">{props.body}</p>
+                <p className="w-2/3 text-base font-mono my-4 text-left">{props.body}</p>
             </div>
         </div>
     );
