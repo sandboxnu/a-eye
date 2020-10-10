f983e0ced4a46fb5745bc66626430afb5eaafed9
Dekenius <dankrasno@gmail.com>
2020-10-09 20:40:14 -0400

    forgot to push cahnges

--- src/kmeans.js
+++ src/kmeans.js
@@ -76,43 +76,48 @@ const MyScatter =
         };
         return (<Scatter data={data} options={options} />);
     };
-class ScatterChart extends Component {
-  constructor(props) {
-    super(props);
-    // console.log(props)
-    this.data = props['props']
-    this.chartReference = React.createRef();
+// class ScatterChart extends Component {
+//   constructor(props) {
+//     super(props);
+//     // console.log(props)
+//     this.data = props['props']
+//     this.chartReference = React.createRef();
 
-  }
-  setState() {
-    console.log("hi")
-  }
+//   }
+//   setState() {
+//     console.log("hi")
+//   }
 
-  render() {
-    return (<MyScatter2 datasetsx={this.data} />)
-  }
-}
+//   render() {
+//     return (<MyScatter2 datasetsx={this.data} />)
+//   }
+// }
 
 const MyScatter2 =
 (props) => {
-  
+  let chartRef = React.createRef();
 
   // in order to make the chart updateable after moving a center
   const [x1Idx, setX1Idx] = useState(props.cntrds[0].centroid[0]);
   const [y1Idx, setY1Idx] = useState(props.cntrds[0].centroid[1]);
   const [x2Idx, setX2Idx] = useState(props.cntrds[1].centroid[0]);
   const [y2Idx, setY2Idx] = useState(props.cntrds[1].centroid[1]);
-
+  let c2 = [[x1Idx, y1Idx], [x2Idx, y2Idx]];
   // points which have been added
   const [addedPoints, setAP]  = useState([]);
-
+  console.log(addedPoints)
+  let trainData2 = trainData.concat(addedPoints)
+  // trainData2.push(addedPoints);
+  
+  console.log(trainData2)
   // format needed for kmeans
-  let c2 = [[x1Idx, y1Idx], [x2Idx, y2Idx]];
+  
 
   // where our data is going to be, 'bubble data'
   let bubData = []
+  console.log(trainData)
 
-  let ans_x = kmeans(organiseData(trainData), k, { initialization: c2, maxIterations: 1 }, );
+  let ans_x = kmeans(organiseData(trainData2), k, { initialization: c2, maxIterations: 1 }, );
   // process data mutates the first argument
   processdata(bubData, ans_x.clusters, c2, props.hidden)
 
@@ -143,14 +148,48 @@ const MyScatter2 =
       //   }
       // },
       onClick : function (evt, item) {
-        console.log(evt);
-        // console.log(item);
+        // console.log(evt);
+        // // const myChartRef = chartRef.current.getContext("2d");
+        // console.log(this);
         // console.log(evt.x - 737, 140 - evt.y)
+        var yTop = this.chartArea.top;
+        var yBottom = this.chartArea.bottom;
+
+        var yMin = this.scales['y-axis-1'].min;
+        var yMax = this.scales['y-axis-1'].max;
+        var newY = 0;
+
+        if (evt.offsetY <= yBottom && evt.offsetY >= yTop) {
+            newY = Math.abs((evt.offsetY - yTop) / (yBottom - yTop));
+            newY = (newY - 1) * -1;
+            newY = newY * (Math.abs(yMax - yMin)) + yMin;
+        };
+
+        var xTop = this.chartArea.left;
+        var xBottom = this.chartArea.right;
+        var xMin = this.scales['x-axis-1'].min;
+        var xMax = this.scales['x-axis-1'].max;
+        var newX = 0;
+
+        if (evt.offsetX <= xBottom && evt.offsetX >= xTop) {
+            newX = Math.abs((evt.offsetX - xTop) / (xBottom - xTop));
+            newX = newX * (Math.abs(xMax - xMin)) + xMin;
+        };
+        newX = Math.round(newX)
+        newY = Math.round(newY)
+
+        // console.log(newX, newY);
+        console.log(addedPoints)
+        let a = addedPoints
+        console.log(a)
+        addedPoints.push({Driver_ID: 0, Distance_Feature: newX, Speeding_Feature: newY})
+        console.log(a)
+        setAP(a)
         
       },
       onDragEnd: function (e, datasetIndex, index, value) {
-        console.log(value)
-        console.log(datasetIndex, index)
+        // console.log(value)
+        // console.log(datasetIndex, index)
         if (datasetIndex == 0) {
           setX1Idx(value.x)
           setY1Idx(value.y)
