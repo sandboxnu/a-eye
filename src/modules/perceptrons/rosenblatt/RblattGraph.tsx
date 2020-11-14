771c4f0d1d20382dba0eafa0f17c0b2456078d4c
Iris Liu <Iris.Lamb2@gmail.com>
2020-11-14 15:54:18 -0500

    start implementing point editing

--- src/modules/perceptrons/rosenblatt/RblattGraph.tsx
+++ src/modules/perceptrons/rosenblatt/RblattGraph.tsx
@@ -2,8 +2,15 @@ import React, { useEffect, useState } from 'react';
 import { RblattInput, RblattConfig } from './RosenblattDemo';
 const JXG = require('jsxgraph');
 
+type RblattGraphProps = { 
+    inputs: RblattInput[], 
+    line: RblattConfig, 
+    highlighted: RblattInput,
+    editingType: 0 | 1 | null, 
+    onInputsChange: (inpts: React.SetStateAction<RblattInput[]>) => void
+ }
 
-const RblattGraph = (props: { inputs: RblattInput[], line: RblattConfig, highlighted: RblattInput }) => {
+const RblattGraph = (props: RblattGraphProps) => {
     const [brdId, setBrdId] = useState('board_' + Math.random().toString(36).substr(2, 9));
     const [board, setBoard] = useState<any>(null);
     const [pointA, setPointA] = useState<any>(null); // 2 points to define the line
@@ -12,9 +19,11 @@ const RblattGraph = (props: { inputs: RblattInput[], line: RblattConfig, highlig
     useEffect(() => {
         const newBoard = JXG.JSXGraph.initBoard(brdId, { boundingbox: [-10, 10, 10, -10], axis: true });
         setBoard(newBoard);
-        props.inputs.forEach(inpt => {
+        props.inputs.forEach((inpt, idx) => {
             const color = inpt.z === 1 ? COL_1 : COL_0
-            newBoard.create('point', [inpt.x, inpt.y], { name: '', size: 1, color, fixed: true });
+            const p = newBoard.create('point', [inpt.x, inpt.y], 
+                                      { name: '', size: 1, color, fixed: true });
+            p.on('down', () => removePoint(p, idx, newBoard));
         });
         const {aCoords, bCoords} = getLinePoints(props.line);
         const pA = newBoard.create('point', aCoords, { name: '', fixed: true, color: 'transparent'});
@@ -27,7 +36,7 @@ const RblattGraph = (props: { inputs: RblattInput[], line: RblattConfig, highlig
     }, []);
 
     // register listeners after the state var has been set
-    //useEffect(() => { board && board.on('down', addPoint) }, [board]);
+    useEffect(() => { board && board.on('down', addPoint) }, [board]);
 
     useEffect(() => {
         const {aCoords, bCoords} = getLinePoints(props.line);
@@ -45,30 +54,47 @@ const RblattGraph = (props: { inputs: RblattInput[], line: RblattConfig, highlig
         }).setAttribute({size: 4});
     }, [props.highlighted]);
 
-    // todo: draw all points thru a useEffect depending on inputs. make the points controlled
     const addPoint = (e: any) => {
-        console.log(board);
-        var canCreate = true, i, coords, el;
+        if (props.editingType === null) return;
+
+        var canCreate = true, i, coords: any, el;
         if (e[JXG.touchProperty]) {
             i = 0;
         }
         coords = getMouseCoords(board, e, i);
-
         for (el in board.objects) {
             if (JXG.isPoint(board.objects[el]) && board.objects[el].hasPoint(coords.scrCoords[1], coords.scrCoords[2])) {
                 canCreate = false;
                 break;
             }
         }
-
         if (canCreate) {
-            board.create('point', [coords.usrCoords[1], coords.usrCoords[2]],
-                { name: '', size: 1, color: '#394d73' });
+            const newIdx = props.inputs.length;
+            props.onInputsChange(oldInpts => {
+                const z = props.editingType || 0;
+                oldInpts.push({x: coords.usrCoords[1], y: coords.usrCoords[2], z });
+                return oldInpts;
+            });
+            // creating points here is *technically* going against controlled components
+            // shhh do not look
+            const p = board.create('point', [coords.usrCoords[1], coords.usrCoords[2]],
+                { name: '', size: 1, color: props.editingType ? COL_1 : COL_0 });
+            p.on('down', () => removePoint(p, newIdx));
+
         }
     }
 
+    const removePoint = (point: any, idx: number, currBoard?: any) => {
+        if (props.editingType === null) return;
+        props.onInputsChange(oldInpts => oldInpts.filter(inpt => inpt.x != point.X() && inpt.y != point.Y()));
+
+        // can't directly use state var for board bc of closures
+        if (!currBoard) currBoard = board;
+        currBoard.removeObject(point); 
+    }
+
     return (
-        <div id={brdId} style={{ width: '500px', height: '500px' }} />
+        <div className="bg-white" id={brdId} style={{ width: '500px', height: '500px' }} />
     );
 }
 
--- src/modules/perceptrons/rosenblatt/RblattNeuron.tsx
+++ src/modules/perceptrons/rosenblatt/RblattNeuron.tsx
@@ -1,8 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import {InputLines} from '../mpNeuron/MPNeuron';
 import {RblattConfig, RblattInput} from './RosenblattDemo';
 
 // todo: make output colors match orange/blue
+// actually that doesnt work bc the output color changes based on the updated config
+// so you never see any error predictions
 
 
 // a static version of the neuron demo, for the Rblatt demo
@@ -16,15 +18,14 @@ const RblattNeuron = (props: {input: RblattInput, config: RblattConfig}) => {
             <div className="flex items-center">
                 {label}
                 <div
-                    className="font-bold rounded-full w-12 h-12 bg-navy m-1
-                                flex items-center justify-center text-white"
-                    style={{ backgroundColor: INPT_CLR }}
+                    className="font-bold rounded-full w-12 h-12 m-1 border-2 border-pink-700 bg-white
+                                flex items-center justify-center"
                 >
                     {val.toFixed(1)}
                 </div>
                 <div className="w-2 h-1 bg-navy" />
                 <div className="m-1">
-                    <input className="number-input w-16 border-2 border-pink-700"
+                    <input className="number-input w-16 border-none bg-pink-700 font-bold text-white py-1"
                         type="number" readOnly={true}
                         value={weight.toFixed(1)}
                     />
@@ -41,39 +42,32 @@ const RblattNeuron = (props: {input: RblattInput, config: RblattConfig}) => {
                 <div className="flex items-center self-end">
                     bias
                     <div
-                        className="font-bold rounded-full w-12 h-12 bg-navy m-1
+                        className="font-bold rounded-full w-12 h-12 bg-pink-700 m-1
                                     flex items-center justify-center text-white"
-                        style={{ backgroundColor: INPT_CLR }}
                     >
                         {props.config.bias.toFixed(1)}
                 </div>
             </div>
             </div>
             <InputLines numInpts={3} />
-            <div className="rounded-full w-20 h-20 bg-brightOrange 
+            <div className="rounded-full w-20 h-20 bg-white border-2 border-orange-500 
                 flex items-center justify-center">
                 {inputSum.toFixed(1)}
             </div>
             <div className="w-2 h-1 bg-navy" />
-            <div className="font-bold w-20 h-10 rounded-md border-2 border-orange-500 
+            <div className="w-20 h-10 rounded-md border-2 border-orange-500 
                         flex items-center justify-center px-2">
                 {'> 0'}
             </div>
             <div className="w-16 h-1 bg-navy" />
             <div
-                className="rounded-full w-12 h-12 font-bold bg-moduleTeal flex items-center justify-center"
-                style={{
-                    backgroundColor: output === 1 ? OUTPT_CLR : 'white',
-                    border: output === 1 ? 'none' : `2px solid ${OUTPT_CLR}`
-                }}
+                className="rounded-full w-12 h-12 font-bold border-2 border-moduleTeal bg-white 
+                        flex items-center justify-center"
             >
                 {output}
             </div>
         </div>
     );
 }
-
-const INPT_CLR = '#b92079';
-const OUTPT_CLR = '#0FD4C0';
 
 export default RblattNeuron;
