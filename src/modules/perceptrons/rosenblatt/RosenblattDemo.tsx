50d830ff5bb4ff85de33978fdf1786f27ade346b
Iris Liu <Iris.Lamb2@gmail.com>
2020-11-15 15:15:23 -0500

    Get point editing working

--- src/modules/perceptrons/rosenblatt/RosenblattDemo.tsx
+++ src/modules/perceptrons/rosenblatt/RosenblattDemo.tsx
@@ -92,20 +92,27 @@ export default RosenBlattDemo;
 const EditingRblattGraph = (props: {inputs: RblattInput[], line: RblattConfig,  highlighted: RblattInput,
                             onInputsChange: (inpts: React.SetStateAction<RblattInput[]>) => void}) => 
 {
-    const [editingType, setEditingType] = useState<1 | 0 | null>(null);
-
+    const [editingType, setEditingType] = useState<{val: 1 | 0 | null}>({val: null});
+    const [updated, setUpdated] = useState(false); // yes this is a hack to get it to rerender shhh do not look
+    
     return (
     <div className="flex flex-col items-center justify-center">
         <RblattGraph {...props} editingType={editingType} />
         <div className="flex items-center justify-center">
             Add/Remove Points:
-            <button className="basic-button alt py-1 px-2"
-                onClick={() => editingType === 0 ? setEditingType(null) : setEditingType(0)}
+            <button className={`basic-button alt py-1 px-2 bg-orange-500 border-4 ${editingType.val === 0 ? 'border-orange-800' : 'border-transparent'} `}
+                onClick={() => {
+                    editingType.val === 0 ? setEditingType(et => {et.val = null; return et}) : setEditingType(et => {et.val = 0; return et});
+                    setUpdated(!updated);
+                }}
             >
                 Orange
             </button>
-            <button className="basic-button py-1 px-2"
-                onClick={() => editingType === 1 ? setEditingType(null) : setEditingType(1)}
+            <button className={`basic-button py-1 px-2 bg-lightNavy border-4 ${editingType.val === 1 ? 'border-blue-900' : 'border-transparent'}`}
+                onClick={() => {
+                    editingType.val === 1 ? setEditingType(et => {et.val = null; return et}) : setEditingType(et => {et.val = 1; return et});
+                    setUpdated(!updated);
+                }}
             >
                 Blue
             </button>
