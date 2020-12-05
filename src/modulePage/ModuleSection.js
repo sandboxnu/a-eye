9c54214702568f9b91cb938dccca6caacf24520a
Max Pinheiro <maxpinheiro181@gmail.com>
2020-12-05 11:56:06 -0500

    add MP neuron to module page

--- src/modulePage/ModuleSection.js
+++ src/modulePage/ModuleSection.js
@@ -8,6 +8,7 @@ import PCADemo from "../modules/stateSpaces/pca/PCA";
 import {RawDataTable, SelectableAxisChart, StaticAxisChart, AxisSelector, config as pcaConfig} from "../modules/stateSpaces/pca/PCA";
 import KMeans, {MyDemo, MyScatter2, config as kmeansConfig} from '../modules/stateSpaces/kmeans/kmeans';
 import RblattVectorsDemo from '../modules/perceptrons/rblattVectors/RblattVectorsDemo';
+import MPNeuron from "../modules/perceptrons/mpNeuron/MPNeuron";
 
 const lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
@@ -85,6 +86,8 @@ function getDemo(comp, scheme) {
             return <KMeans />
         case "RblattVectorsDemo":
             return <RblattVectorsDemo />
+        case "MPNeuron":
+            return <MPNeuron />
         default: return <div></div>
     }
 }
