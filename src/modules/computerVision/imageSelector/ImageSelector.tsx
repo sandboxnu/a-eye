247017d726ad75c115d231382743a87faa6b6891
stanleykywu <stanleykywu@gmail.com>
2021-01-31 14:23:59 -0500

    Move imageSelector images into media

--- src/modules/computerVision/imageSelector/ImageSelector.tsx
+++ src/modules/computerVision/imageSelector/ImageSelector.tsx
@@ -29,18 +29,18 @@ const ImageSelector = (props: { currImg: string, onSelect: (img: string, imgUrl:
 
 
 const ALL_IMGS: { [name: string]: any } = {
-    'three.png': require('./imageLibrary/three.png'),
-    'diamond.png': require('./imageLibrary/diamond.png'),
-    'square.png': require('./imageLibrary/square.png'),
-    'circles.jpg': require('./imageLibrary/circles.jpg'),
-    'dogSilhouette.jpg': require('./imageLibrary/dogSilhouette.jpg'),
-    'purpleFlowers.jpeg': require('./imageLibrary/purpleFlowers.jpeg'),
-    'steps.png': require('./imageLibrary/steps.png'),
-    'tabbyCat.jpg': require('./imageLibrary/tabbyCat.jpg'),
-    'teddyBear.jpg': require('./imageLibrary/teddyBear.jpg'),
-    'zebra.jpg': require('./imageLibrary/zebra.jpg'),
-    'bwWoman.jpg': require('./imageLibrary/bwWoman.jpg'),
-    'bwMan.jpg': require('./imageLibrary/bwMan.jpg')
+    'three.png': require('../../../media/modules/computerVision/imageLibrary/three.png'),
+    'diamond.png': require('../../../media/modules/computerVision/imageLibrary/diamond.png'),
+    'square.png': require('../../../media/modules/computerVision/imageLibrary/square.png'),
+    'circles.jpg': require('../../../media/modules/computerVision/imageLibrary/circles.jpg'),
+    'dogSilhouette.jpg': require('../../../media/modules/computerVision/imageLibrary/dogSilhouette.jpg'),
+    'purpleFlowers.jpeg': require('../../../media/modules/computerVision/imageLibrary/purpleFlowers.jpeg'),
+    'steps.png': require('../../../media/modules/computerVision/imageLibrary/steps.png'),
+    'tabbyCat.jpg': require('../../../media/modules/computerVision/imageLibrary/tabbyCat.jpg'),
+    'teddyBear.jpg': require('../../../media/modules/computerVision/imageLibrary/teddyBear.jpg'),
+    'zebra.jpg': require('../../../media/modules/computerVision/imageLibrary/zebra.jpg'),
+    'bwWoman.jpg': require('../../../media/modules/computerVision/imageLibrary/bwWoman.jpg'),
+    'bwMan.jpg': require('../../../media/modules/computerVision/imageLibrary/bwMan.jpg')
 }
 
 export default ImageSelector;
