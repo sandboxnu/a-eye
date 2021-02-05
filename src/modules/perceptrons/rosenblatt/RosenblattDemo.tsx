10501a93f97e597a6080dc6cef7d0de6f48fb8c2
Jacob Chvatal <jake@isnt.online>
2021-02-05 14:11:54 -0800

    Fix bug adding points twice to rblatt

--- src/modules/perceptrons/rosenblatt/RosenblattDemo.tsx
+++ src/modules/perceptrons/rosenblatt/RosenblattDemo.tsx
@@ -3,9 +3,11 @@ import RblattGraph from './RblattGraph';
 import RblattInputsTable from './RblattInputsTable';
 import RblattNeuron from './RblattNeuron';
 import distanceToLineSegment from 'distance-to-line-segment';
+import {RblattInput, RblattConfig, INIT_INPUTS, INIT_CONFIG} from './constants';
 
-export type RblattInput = { x: number, y: number, z: 0 | 1 };
-export type RblattConfig = { weightX: number, weightY: number, bias: number, learningRate: number };
+const round = (x: number, length: number) => {
+    return Math.round(x * (10 ** length)) / (10 ** length);
+}
 
 const RosenBlattDemo = (props: { labelColor: string }) => {
     const [inputs, setInputs] = useState<RblattInput[]>(INIT_INPUTS);
@@ -17,8 +19,9 @@ const RosenBlattDemo = (props: { labelColor: string }) => {
 
     useEffect(() => {
         updateErrors(inputs, config);
-    })
+    }, []);
 
+    // 
     const updateErrors = (inputs: RblattInput[], config: RblattConfig) => {
         let binCount = 0;
         let msCount = 0
@@ -80,11 +83,8 @@ const RosenBlattDemo = (props: { labelColor: string }) => {
 
     const resetConfig = () => {
         setConfig(INIT_CONFIG);
+        setInputs(INIT_INPUTS);
         setCurrPoint(0);
-    }
-
-    const round = (x: number, length: number) => {
-        return Math.round(x * (10 ** length)) / (10 ** length);
     }
 
     return (
@@ -148,14 +148,14 @@ export default RosenBlattDemo;
 const EditingRblattGraph = (props: {inputs: RblattInput[], line: RblattConfig,  highlighted: RblattInput,
                             onInputsChange: (inpts: React.SetStateAction<RblattInput[]>) => void}) => 
 {
-    const [editingType, setEditingType] = useState<{val: 1 | 0 | null}>({val: null});
+    const [editingType, setEditingType] = useState<{val: 1 | 0 | null}>({val: 0});
     const [updated, setUpdated] = useState(false); // yes this is a hack to get it to rerender shhh do not look
     
     return (
     <div className="flex flex-col items-center justify-center">
         <RblattGraph {...props} editingType={editingType} />
         <div className="flex items-center justify-center">
-            <p className="text-modulePaleBlue">Add/Remove Points:</p>
+            <p className="text-modulePaleBlue">Select Point Color:</p>
             <button className={`basic-button alt py-1 px-2 bg-orange-500 border-4 ${editingType.val === 0 ? 'border-orange-800' : 'border-transparent'} `}
                 onClick={() => {
                     editingType.val === 0 ? setEditingType(et => {et.val = null; return et}) : setEditingType(et => {et.val = 0; return et});
@@ -190,23 +190,3 @@ function trainRblatt(inpt: RblattInput, config: RblattConfig) {
         return { ...config, error }
     }
 }
-
-const INIT_CONFIG = { weightX: .7, weightY: -1, bias: 0, learningRate: .1 };
-const INIT_INPUTS: RblattInput[] = [
-    { x: 2.101231155778894, y: 4.947319932998326, z: 0 },
-    { x: 8.27, y: -3.94, z: 0 },
-    { x: 0.7838107202680059, y: 4.2886097152428815, z: 0 },
-    { x: 2.7711055276381822, y: 9.000921273031828, z: 0 },
-    { x: 5.344112227805695, y: 5.910050251256282, z: 0 },
-    { x: 5.445452261306533, y: 1.1977386934673367, z: 0 },
-    { x: 4.482721943048576, y: -0.7783919597989952, z: 0 },
-    { x: 7.725603015075377, y: -3.05854271356784, z: 0 },
-    { x: 5.445452261306533, y: -3.210552763819096, z: 0 },
-    { x: -0.28025963149078823, y: 2.0084589614740372, z: 1 },
-    { x: 2.506591289782244, y: -3.6159128978224464, z: 1 },
-    { x: -2.2057202680067015, y: -0.4237018425460638, z: 1 },
-    { x: -3.573810720268008, y: 2.819179229480737, z: 1 },
-    { x: -5.549941373534341, y: 2.211139028475712, z: 1 },
-    { x: 0.5304606365159121, y: -3.109212730318259, z: 1 },
-    { x: -4.485871021775546, y: -3.1598827470686774, z: 1 }
-];
