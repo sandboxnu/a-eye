c8cb69d8672ccdb0d3d1d93ce9ed1fce1b961946
Max Pinheiro <maxpinheiro181@gmail.com>
2021-02-06 11:30:35 -0500

    started vector translation

--- src/modules/perceptrons/rblattVectors/RblattVectorsDemo.tsx
+++ src/modules/perceptrons/rblattVectors/RblattVectorsDemo.tsx
@@ -9,24 +9,25 @@ const RblattVectorsDemo = () => {
     const [perpPoint, setPerpPoint] = useState<any>(null);
     const phaseMessages = [
         'The initial classification line is represented by its perpendicular vector Wt.',
-        'After finding a misclassified point, we calculate the distance d between it and the vector\'s origin/',
+        'After finding a misclassified point, we calculate the distance d between it and the vector\'s origin.',
         'We then calculate the new vector by subtracting d from Wt.',
         'Finally, we update the classification line to be perpendicular to the new vector (Wt - d).'];
     const [brdMsg, setBrdMsg] = useState<string>(phaseMessages[0]);
+    const [demoConfig, setDemoConfig] = useState<any>(rotationConfig);
 
     useEffect(() => {
         const newBoard = JXG.JSXGraph.initBoard(brdId, { boundingbox: [2, 6, 7, 0], axis: true });
         setBoard(newBoard);
-        POINTS.forEach((inpt) => {
+        demoConfig.POINTS.forEach((inpt) => {
             const color = inpt.z === 1 ? COL_1 : COL_0
             const p = newBoard.create('point', [inpt.x, inpt.y], 
                                       { name: '', size: 1, color, fixed: true });
         });
-        const pA = newBoard.create('point', INIT_PERP_POINT, { name: '', fixed: true, color: 'transparent'});
+        const pA = newBoard.create('point', demoConfig.INIT_PERP_POINT, { name: '', fixed: true, color: 'transparent'});
         setPerpPoint(pA);
-        const perpArrow = newBoard.create('arrow', [MAINLINE_INTERSECTION, pA], 
+        const perpArrow = newBoard.create('arrow', [demoConfig.MAINLINE_INTERSECTION, pA],
             { strokeColor: 'black', strokeWidth: 2, fixed: true, name: 'Wt', withLabel:true, label: {position: 'top'} });
-        const li = newBoard.create('perpendicular', [perpArrow, MAINLINE_INTERSECTION], 
+        const li = newBoard.create('perpendicular', [perpArrow, demoConfig.MAINLINE_INTERSECTION],
             { name: 'mainLine',  withLabel:false, strokeColor: 'black', strokeWidth: 2, fixed: true });
         newBoard.create('inequality', [li], {name: 'ineq1', fillColor: COL_0});
         newBoard.create('inequality', [li], {name: 'ineq2', inverse: true, fillColor: COL_1 });
@@ -38,30 +39,30 @@ const RblattVectorsDemo = () => {
         switch (phase) {
             case 0:
                 board?.select({
-                    X: (v: number) => v === CURR_POINT[0], 
-                    Y: (v: number) => v === CURR_POINT[1]
+                    X: (v: number) => v === demoConfig.CURR_POINT[0],
+                    Y: (v: number) => v === demoConfig.CURR_POINT[1]
                 }).setAttribute({size: 4});
-                const dist = board?.create('arrow', [MAINLINE_INTERSECTION, CURR_POINT], 
+                const dist = board?.create('arrow', [demoConfig.MAINLINE_INTERSECTION, demoConfig.CURR_POINT],
                     { strokeColor: 'black', strokeWidth: 2, fixed: true, dash: 2, name: 'd', withLabel:true, label: {position: 'top'}});
                 setPhase(1);
                 break;
             case 1:
-                board?.create('arrow', [INIT_PERP_POINT, NEW_PERP_POINT],
+                board?.create('arrow', [demoConfig.INIT_PERP_POINT, demoConfig.NEW_PERP_POINT],
                      { strokeColor: 'black', strokeWidth: 2, fixed: true, dash: 2, name: '-d', withLabel:true, label: {position: 'top'}});
-                board?.create('arrow', [MAINLINE_INTERSECTION, NEW_PERP_POINT], 
+                board?.create('arrow', [demoConfig.MAINLINE_INTERSECTION, demoConfig.NEW_PERP_POINT],
                     { strokeColor: 'black', strokeWidth: 2, fixed: true, dash: 3, name: 'Wt - d', withLabel:true, label: {position: 'top'}});
                 setPhase(2);
                 break;
             case 2:
                 board?.select({
-                    X: (v: number) => v === CURR_POINT[0], 
-                    Y: (v: number) => v === CURR_POINT[1]
+                    X: (v: number) => v === demoConfig.CURR_POINT[0],
+                    Y: (v: number) => v === demoConfig.CURR_POINT[1]
                 }).setAttribute({size: 1});
                 board?.removeObject('d');
                 board?.removeObject('-d');
                 board?.removeObject('ineq1');
                 board?.removeObject('ineq2');
-                perpPoint.moveTo(NEW_PERP_POINT, 700, {callback: () => {
+                perpPoint.moveTo(demoConfig.NEW_PERP_POINT, 700, {callback: () => {
                     board?.create('inequality', ['mainLine'], {name: 'ineq1', fillColor: COL_0});
                     board?.create('inequality', ['mainLine'], {name: 'ineq2', inverse: true, fillColor: COL_1 });
                 }});
@@ -75,8 +76,8 @@ const RblattVectorsDemo = () => {
         switch (phase) {
             case 1:
                 board?.select({
-                    X: (v: number) => v === CURR_POINT[0], 
-                    Y: (v: number) => v === CURR_POINT[1]
+                    X: (v: number) => v === demoConfig.CURR_POINT[0],
+                    Y: (v: number) => v === demoConfig.CURR_POINT[1]
                 }).setAttribute({size: 1});
                 board?.removeObject('d');
                 setPhase(0);
@@ -89,12 +90,12 @@ const RblattVectorsDemo = () => {
             case 3:
                 board?.removeObject('ineq1');
                 board?.removeObject('ineq2');
-                perpPoint.moveTo(INIT_PERP_POINT, 700, {callback: () => {
+                perpPoint.moveTo(demoConfig.INIT_PERP_POINT, 700, {callback: () => {
                     board?.create('inequality', ['mainLine'], {name: 'ineq1', inverse: true, fillColor: COL_0});
                     board?.create('inequality', ['mainLine'], {name: 'ineq2', fillColor: COL_1 });
-                    board?.create('arrow', [MAINLINE_INTERSECTION, CURR_POINT],
+                    board?.create('arrow', [demoConfig.MAINLINE_INTERSECTION, demoConfig.CURR_POINT],
                     { strokeColor: 'black', strokeWidth: 2, fixed: true, dash: 2, name: 'd', withLabel:true, label: {position: 'top'}});
-                    board?.create('arrow', [INIT_PERP_POINT, NEW_PERP_POINT],
+                    board?.create('arrow', [demoConfig.INIT_PERP_POINT, demoConfig.NEW_PERP_POINT],
                      { strokeColor: 'black', strokeWidth: 2, fixed: true, dash: 2, name: '-d', withLabel:true, label: {position: 'top'}});
                 }});
                 setPhase(2);
@@ -120,15 +121,30 @@ export default RblattVectorsDemo;
 
 const COL_0 = '#f15e2c';
 const COL_1 = '#394d73';
-const POINTS = [
-    {x: 3.481875, y: 4.48, z: 0},
-    {x: 5.161875, y: 4.52, z: 0},
-    {x: 4.801875, y: 3.44, z: 0},
-    {x: 3.161875, y: 0.88, z: 1},
-    {x: 3.721875, y: 1.84, z: 1},
-    {x: 5.281875, y: .96, z: 1}];
-const INIT_PERP_POINT = [6, 2.5];
-const NEW_PERP_POINT = [5.22, 4.04];
-const MAINLINE_INTERSECTION = [4.5, 2.5];
-const CURR_POINT = [ 5.281875, .96];
+const rotationConfig = {
+    POINTS: [
+        {x: 3.481875, y: 4.48, z: 0},
+        {x: 5.161875, y: 4.52, z: 0},
+        {x: 4.801875, y: 3.44, z: 0},
+        {x: 3.161875, y: 0.88, z: 1},
+        {x: 3.721875, y: 1.84, z: 1},
+        {x: 5.281875, y: .96, z: 1}],
+    INIT_PERP_POINT: [6, 2.5],
+    NEW_PERP_POINT: [5.22, 4.04],
+    MAINLINE_INTERSECTION: [4.5, 2.5],
+    CURR_POINT: [5.281875, .96]
+};
+const translationConfig = {
+    POINTS: [
+        {x: 3.481875, y: 4.48, z: 0},
+        {x: 5.161875, y: 4.52, z: 0},
+        {x: 4.801875, y: 3.44, z: 1},
+        {x: 3.161875, y: 0.88, z: 1},
+        {x: 3.721875, y: 1.84, z: 1},
+        {x: 5.281875, y: .96, z: 1}],
+    INIT_PERP_POINT: [5.22, 4.04],
+    NEW_PERP_POINT: [5.22, 4.04],
+    MAINLINE_INTERSECTION: [4.5, 2.5],
+    CURR_POINT: [4.801875, 3.44]
+};
 
