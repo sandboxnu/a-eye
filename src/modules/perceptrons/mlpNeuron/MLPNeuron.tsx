cefd354b4f927562d6c92a9dfdfbbf5aaf798ecf
Jacob Chvatal <jake@isnt.online>
2021-03-06 18:03:05 -0800

    allow parameter changes on perceptron

--- src/modules/perceptrons/mlpNeuron/MLPNeuron.tsx
+++ src/modules/perceptrons/mlpNeuron/MLPNeuron.tsx
@@ -6,30 +6,87 @@ const MLPNeuron = (props: { labelColor: string }) => {
 
     const defaultInput = [{ val: 1, weight: 10 }];
 
+    const initialWeights = [-.5, 1];
+
     const [answer1, setAns1] = useState(0);
     const [answer2, setAns2] = useState(0);
 
-    const initialWeights = [-.5, 1];
-
     const [weights, setWeights] = useState<number[]>(initialWeights);
+
     const [inputs, setInputs] = useState<NeuronInput[]>(defaultInput);
     const [inputs2, setInputs2] = useState<NeuronInput[]>(defaultInput);
+
+    const [threshold1, setThreshold1] = useState(0);
+    const [isGreater1, setIsGreater1] = useState(true);
+
+    const [threshold2, setThreshold2] = useState(0);
+    const [isGreater2, setIsGreater2] = useState(true);
+
+    const [thresholdLayer, setThresholdLayer] = useState(0);
+    const [isGreaterLayer, setIsGreaterLayer] = useState(true);
+
+    const [bias, setBias] = useState(10);
+
+    const resetDemo = () => {
+        setWeights([10, 10]);
+        setInputs(inputs);
+        setInputs2(inputs2);
+        setThreshold1(0);
+        setIsGreater1(true);
+        setThreshold2(0);
+        setIsGreater2(true);
+        setThresholdLayer(0);
+        setIsGreaterLayer(true);
+    }
+
+    const setAnd = () => {
+        // reset all weights
+        // reset threshold
+        // keep inputs
+        resetDemo();
+        setBias(-15);
+    }
+
+    const setOr = () => {
+        resetDemo();
+        setBias(-5);
+    }
 
     return (
         <div>
             <div className="m-2 flex items-center">
                 <div>
-                    <MPBasicNeuron labelColor={props.labelColor} canAddInputs={false} addBias={true} onAnsChange={setAns1} inputs={inputs} setInputs={setInputs} />
-                    <MPBasicNeuron labelColor={props.labelColor} canAddInputs={false} addBias={true} onAnsChange={setAns2} inputs={inputs2} setInputs={setInputs2} />
+                    <MPBasicNeuron 
+                    threshold={threshold1}
+                    setThreshold={setThreshold1}
+                    isGreater={isGreater1}
+                    setIsGreater={setIsGreater1}
+                    labelColor={props.labelColor} canAddInputs={false} addBias={true} onAnsChange={setAns1} inputs={inputs} setInputs={setInputs} />
+                    <MPBasicNeuron 
+                    threshold={threshold2}
+                    setThreshold={setThreshold2}
+                    isGreater={isGreater2}
+                    setIsGreater={setIsGreater2}
+                    labelColor={props.labelColor} canAddInputs={false} addBias={true} onAnsChange={setAns2} inputs={inputs2} setInputs={setInputs2} />
                 </div>
                 <div className="">
                     <MPLayerNeuron
-                        labelColor={props.labelColor} addBias={true} inputs={[answer1, answer2]} weights={weights} setWeights={setWeights} />
+                        threshold={thresholdLayer}
+                        setThreshold={setThresholdLayer}
+                        isGreater={isGreaterLayer}
+                        setIsGreater={setIsGreaterLayer}
+                        labelColor={props.labelColor} addBias={true} inputs={[answer1, answer2]} weights={weights} setWeights={setWeights} 
+                        bias={bias} 
+                       />
                 </div>
             </div>
             <div>
-                <button className="bg-white">
-                    click me.
+                <button className="bg-white" onClick={setAnd}>
+                    Set to And
+                </button>
+                <br/>
+                <button className="bg-white" onClick={setOr}>
+                    Set to Or
                 </button>
             </div>
         </div>
