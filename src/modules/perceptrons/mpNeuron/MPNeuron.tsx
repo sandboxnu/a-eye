8474a51127270191899ebcc957390da6e841973c
Jacob Chvatal <jake@isnt.online>
2021-03-06 16:30:45 -0800

    add layer neuron

--- src/modules/perceptrons/mpNeuron/MPNeuron.tsx
+++ src/modules/perceptrons/mpNeuron/MPNeuron.tsx
@@ -2,38 +2,40 @@ import React, { useEffect, useState } from 'react';
 import { AddCircle, RemoveCircle } from '@material-ui/icons';
 import { INIT_CONFIG } from '../rosenblatt/constants';
 
+const zip = (arr1: Array<any>, arr2: Array<any>) => {
+    return arr1.map(function (e, i) {
+        return [e, arr2[i]];
+    });
+}
+
 export type NeuronInput = {
     val: number | null,
     weight: number | null
 }
 
 export type MPNeuronType = {
-    labelColor:string,
+    labelColor: string,
     canAddInputs?: boolean,
-    addBias?:boolean,
-    input?:{val:number, weight:number}[],
+    addBias?: boolean,
+    input?: { val: number, weight: number }[],
     onAnsChange?: (inpts: React.SetStateAction<number>) => void,
-    connecting?:boolean
+    connecting?: boolean
 }
 
-export const MPNeuron:React.FC<MPNeuronType> = ({
+export const MPNeuron: React.FC<MPNeuronType> = ({
     labelColor,
-    canAddInputs=true,
-    addBias=false,
-    input=[{ val: 1, weight: -.5 },
-        { val: 1, weight: 1 }],
-    onAnsChange= (()=>null),
-    connecting=false
+    canAddInputs = true,
+    addBias = false,
+    input = [{ val: 1, weight: -.5 },
+    { val: 1, weight: 1 }],
+    onAnsChange = (() => null),
+    connecting = false
 }) => {
+    console.log(input);
 
     const [inputs, setInputs] = useState<NeuronInput[]>(input);
     const [func, setFunc] = useState(() => ((n: number) => 0));
-
-    useEffect(()=>{
-        setInputs(input)
-    },
-    [input]
-     )
+    console.log(inputs);
 
     const changeWeight = (e: React.ChangeEvent<HTMLInputElement>, idx: number) => {
         const val = parseFloat(e.target.value);
@@ -75,10 +77,15 @@ export const MPNeuron:React.FC<MPNeuronType> = ({
     }
 
     const inputSum = inputs.reduce((prev, acc) => {
-        return (acc.val && acc.weight ? acc.val * acc.weight : 0) + INIT_CONFIG.bias + prev
-    }, 0);
+        return (acc.val && acc.weight ? acc.val * acc.weight : 0) + prev
+    }, 0) + INIT_CONFIG.bias;
+
     const output = func(inputSum);
+<<<<<<< HEAD
     onAnsChange(output)
+=======
+    onAnsChange(output);
+>>>>>>> 243d6ef (add layer neuron)
 
     const makeInput = (inpt: NeuronInput, idx: number, connecting: boolean) => {
         return (
@@ -88,8 +95,8 @@ export const MPNeuron:React.FC<MPNeuronType> = ({
                         type="number"
                         value={inpt.val !== null ? inpt.val : ''}
                         onChange={(e) => changeVal(e, idx)}
-                  />
-                </div> }
+                    />
+                </div>}
                 <div className="w-10 h-1 bg-navy" />
                 <div className="m-1">
                     <input className="number-input w-20 h-10 border-2 border-pink-700"
@@ -116,10 +123,10 @@ export const MPNeuron:React.FC<MPNeuronType> = ({
                             {INIT_CONFIG.bias.toFixed(1)}
                         </div>
 
-                    </div> }
+                    </div>}
                 </div>
-                
-                <InputLines numInpts={(addBias ? 1 : 0) + inputs.length } />
+
+                <InputLines numInpts={(addBias ? 1 : 0) + inputs.length} />
                 <div className="rounded-full w-20 h-20 bg-brightOrange 
                 flex items-center justify-center">
                     {inputSum}
@@ -139,10 +146,10 @@ export const MPNeuron:React.FC<MPNeuronType> = ({
             </div>
             {canAddInputs && <div>
 
-            <RemoveCircle className="icon-button" fontSize="large" onClick={removeInput}/>
-            <p className="inline m-2 text-white">{inputs.length} inputs</p>
-            <AddCircle className="icon-button" fontSize="large" onClick={addInput}/>
-                </div> }
+                <RemoveCircle className="icon-button" fontSize="large" onClick={removeInput} />
+                <p className="inline m-2 text-white">{inputs.length} inputs</p>
+                <AddCircle className="icon-button" fontSize="large" onClick={addInput} />
+            </div>}
         </div>
     );
 }
@@ -165,6 +172,103 @@ export const InputLines = (props: { numInpts: number }) => {
         </div>
     );
 }
+
+export type MPLayerNeuronType = {
+    labelColor: string,
+    addBias?: boolean,
+    initialWeights: number[],
+    inputs: number[],
+    onAnsChange?: (inpts: React.SetStateAction<number>) => void,
+}
+
+export const MPLayerNeuron: React.FC<MPLayerNeuronType> = ({
+    labelColor,
+    addBias = false,
+    initialWeights = [-.5, 1],
+    inputs,
+    onAnsChange = (() => null),
+}) => {
+    const [weights, setWeights] = useState<number[]>(initialWeights);
+    const [func, setFunc] = useState(() => ((n: number) => 0));
+
+    const changeWeight = (e: React.ChangeEvent<HTMLInputElement>, idx: number) => {
+        const val = parseFloat(e.target.value);
+        const newInputs = JSON.parse(JSON.stringify((weights)));
+        if (!isNaN(val)) {
+            newInputs[idx] = val;
+            setWeights(newInputs);
+        } else if (e.target.value === '') {
+            newInputs[idx] = null;
+            setWeights(newInputs);
+        }
+    }
+
+    const onFuncChange = (func: (n: number) => number) => {
+        setFunc(() => ((n: number) => func(n)));
+    }
+
+    const inputSum = zip(inputs, weights).reduce((prev, acc) => {
+        return (acc[0] && acc[1] ? acc[0] * acc[1] : 0) + prev
+    }, 0) + INIT_CONFIG.bias;
+
+    const output = func(inputSum);
+    onAnsChange(output);
+
+    const makeInput = (inpt: number, idx: number) => {
+        return (
+            <div className="flex items-center cursor-pointer">
+                <div className="w-10 h-1 bg-navy" />
+                <div className="m-1">
+                    <input className="number-input w-20 h-10 border-2 border-pink-700"
+                        type="number"
+                        value={inpt !== null ? inpt : ''}
+                        onChange={(e) => changeWeight(e, idx)}
+                    />
+                </div>
+            </div>
+        );
+    }
+
+    return (
+        <div className="m-2 flex flex-col items-center justify-center">
+            <div className="flex items-center">
+                <div className="flex flex-col">
+                    {weights.map((weight, index) => makeInput(weight, index))}
+                    {addBias && <div className="flex items-center self-end">
+                        <p className={labelColor}>bias</p>
+                        <div
+                            className="font-bold rounded-full w-12 h-12 bg-pink-700 m-1
+                                        flex items-center justify-center text-white"
+                        >
+
+                            {INIT_CONFIG.bias.toFixed(1)}
+                        </div>
+                    </div>}
+                </div>
+
+                <InputLines numInpts={(addBias ? 1 : 0) + inputs.length} />
+                <div className="rounded-full w-20 h-20 bg-brightOrange 
+                flex items-center justify-center">
+                    {inputSum}
+                </div>
+                <div className="w-2 h-1 bg-navy" />
+                <ThresholdFunc onFuncChange={onFuncChange} />
+                <div className="w-16 h-1 bg-navy" />
+                <div
+                    className="rounded-full w-12 h-12 font-bold bg-moduleTeal flex items-center justify-center"
+                    style={{
+                        backgroundColor: output === 1 ? OUTPT_CLR : 'white',
+                        border: output === 1 ? 'none' : `2px solid ${OUTPT_CLR}`
+                    }}
+                >
+                    {output}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+
 
 // outlines
 // both > and < shown, highlight the one you want
