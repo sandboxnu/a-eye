837b13bacd9cce5a4326500317c54644a35fdd25
Max Pinheiro <maxpinheiro181@gmail.com>
2021-03-14 12:47:20 -0400

    calculate histogram of gradients

--- src/modules/computerVision/histOfGrad/histOfGrad.tsx
+++ src/modules/computerVision/histOfGrad/histOfGrad.tsx
@@ -1,13 +1,38 @@
-const opencv = require('./opencv');
-
+const { Image } = require('image-js');
+const hog = require('hog-features');
+const img = require('../../../media/modules/computerVision/imageLibrary/purpleFlowers.jpeg')
+  .default;
 
 function histogram() {
-    let img = opencv.imread('../../media/modules/computerVision/imageLibrary/bwMan.jpg');
-    let gx = opencv.Mat();
-    let gy = opencv.Mat();
+  Image.load(img).then((image: any) => {
+    const options = {
+      cellSize: 8,
+      blockSize: 2,
+      blockStride: 1,
+      bins: 9,
+    };
+    const descriptor = hog.extractHOG(image, options);
+    /* eslint-disable-next-line */
+    const blockWidth = Math.floor((image.width / options.cellSize - options.blockSize) / options.blockStride) + 1;
+    /* eslint-disable-next-line */
+    const blockHeight = Math.floor((image.height / options.cellSize - options.blockSize) / options.blockStride) + 1;
 
-    gx = opencv.Sobel(img, gx, opencv.CV_8U, 1, 0, 1);
-    console.log(gx);
+    const blockHistograms: number[][][] = [];
+    for (let blockRow = 0; blockRow < blockWidth; blockRow += 1) {
+      const row: number[][] = [];
+      for (let blockCol = 0; blockCol < blockHeight; blockCol += 1) {
+        const index = blockRow * blockWidth + blockCol;
+        row.push(
+          descriptor.slice(
+            index * options.bins,
+            index * options.bins + options.bins,
+          ),
+        );
+      }
+      blockHistograms.push(row);
+    }
+    console.log(blockHistograms);
+  });
 }
 
 export default histogram;
