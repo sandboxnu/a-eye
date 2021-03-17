4bba4b3b7ca33ed51f094604f05e5c2c2a0c10e8
Max Pinheiro <maxpinheiro181@gmail.com>
2021-03-17 17:48:54 -0400

    calculate alpha for each pixel

--- src/modules/computerVision/histOfGrad/histOfGrad.tsx
+++ src/modules/computerVision/histOfGrad/histOfGrad.tsx
@@ -58,40 +58,47 @@ async function histogramBlocks(img: any): Promise<number[][][]> {
 type Gradients = {
   x: ImageData;
   y: ImageData;
-  m: ImageData;
+  a: number[][];
+  v: ImageData;
 };
 
 const map = (x: number, min1: number, max1: number, min2: number, max2: number): number => (x - min1) * (max2 - min2) / (max1 - min1) + min2;
 
 async function gradientImages(img: any): Promise<Gradients> {
   return Image.load(img).then((image: any) => {
-
     const intensities: { x: number[][], y: number[][] } = hog.gradients(image);
     const intensitiesX: number[] = intensities.x.flat();
     const intensitiesY: number[] = intensities.y.flat();
-    const intensitiesM = intensitiesX.map((num, idx) => Math.sqrt(Math.pow(num, 2) + Math.pow(intensitiesY[idx], 2)));
+    const intensitiesV: number[] = intensitiesX.map((num, idx) => Math.sqrt(Math.pow(num, 2) + Math.pow(intensitiesY[idx], 2)));
+    const intensitiesA: number[] = intensitiesX.map((x, idx) => ((Math.atan(intensitiesY[idx] / x) * (180.0 / Math.PI)) + 180) % 180);
 
-    let maxX = 0, maxY = 0, maxM = 0, minX = 0, minY = 0, minM = 0;
+    let maxX = 0, maxY = 0, maxV = 0, minX = 0, minY = 0, minV = 0;
     intensitiesX.forEach(x => {if (!isNaN(x)) {maxX = Math.max(maxX, x); minX = Math.min(minX, x);}});
     intensitiesY.forEach(y => {if (!isNaN(y)) {maxY = Math.max(maxY, y); minY = Math.min(minY, y);}});
-    intensitiesM.forEach(m => {if (!isNaN(m)) {maxM = Math.max(maxM, m); minM = Math.min(minM, m);}});
+    intensitiesV.forEach(v => {if (!isNaN(v)) {maxV = Math.max(maxV, v); minV = Math.min(minV, v);}});
 
     const valToRGB = (val: number): number[] => [val, val, val, 1];
 
     const imageX: Uint8ClampedArray = new Uint8ClampedArray(intensitiesX.map(x => valToRGB(map(x, minX, maxX, 0, 255))).flat());
     const imageY: Uint8ClampedArray = new Uint8ClampedArray(intensitiesY.map(y => valToRGB(map(y, minY, maxY, 0, 255))).flat());
-    const imageM: Uint8ClampedArray = new Uint8ClampedArray(intensitiesM.map(m => valToRGB(map(m, minM, maxM, 0, 255))).flat());
+    const imageV: Uint8ClampedArray = new Uint8ClampedArray(intensitiesV.map(v => valToRGB(map(v, minV, maxV, 0, 255))).flat());
+
+    const alphas: number[][] = [];
+    for (let row = 0; row < image.height; row++) {
+      alphas.push(intensitiesA.slice(row * image.height, row * image.height + image.width));
+    }
 
     return {
       x: new ImageData(imageX, image.width, image.height),
       y: new ImageData(imageY, image.width, image.height),
-      m: new ImageData(imageM, image.width, image.height),
+      a: alphas,
+      v: new ImageData(imageV, image.width, image.height),
     }
   });
 }
 
 function histogram() {
-  histogramAggregate(flowers).then(result => console.log(result));
+  gradientImages(flowers).then(result => console.log(result));
 }
 
 // todo: what are we doing the 'v' image, clarify what g_x and g_y are
