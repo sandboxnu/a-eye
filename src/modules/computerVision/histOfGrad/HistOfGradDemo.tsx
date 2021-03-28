7e24abf38e4527971e9787a8c9c49d2491bf06b4
Max Pinheiro <maxpinheiro181@gmail.com>
2021-03-28 12:36:17 -0400

    begin needle plot

--- src/modules/computerVision/histOfGrad/HistOfGradDemo.tsx
+++ src/modules/computerVision/histOfGrad/HistOfGradDemo.tsx
@@ -3,7 +3,7 @@
 var Chart = require('chart.js');
 import React, {useLayoutEffect, useRef, useState} from 'react';
 import {ALL_IMGS} from '../imageSelector/ImageSelector';
-import {histogramAggregate, histogramBlocks, gradientImages, GradientsType} from './histOfGrad';
+import {histogramAggregate, histogramBlocks, gradientImages, GradientsType, BlocksType, map} from './histOfGrad';
 
 type HistOfGradDemoType = {
     labelColor: string;
@@ -18,6 +18,7 @@ const HistOfGradDemo: React.FC<HistOfGradDemoType> = ({
     const canvasY = useRef<HTMLCanvasElement>(null);
     const canvasV = useRef<HTMLCanvasElement>(null);
     const canvasHist = useRef<HTMLCanvasElement>(null);
+    const canvasNdlPlt = useRef<HTMLCanvasElement>(null);
     const imgRef = useRef<HTMLImageElement>(null);
     const [imgWidth, setImgWidth] = useState<number>();
     const [imgHeight, setImgHeight] = useState<number>();
@@ -27,8 +28,9 @@ const HistOfGradDemo: React.FC<HistOfGradDemoType> = ({
         const canvasYElem = canvasY.current;
         const canvasVElem = canvasV.current;
         const canvasHistElem = canvasHist.current;
+        const canvasNdlPltElem = canvasNdlPlt.current;
         const imgElem = imgRef.current;
-        if (!(canvasXElem && canvasYElem && canvasVElem && imgElem && canvasHistElem)) return;
+        if (!(canvasXElem && canvasYElem && canvasVElem && imgElem && canvasHistElem && canvasNdlPltElem)) return;
 
         imgElem.onload = () => {
             setImgHeight(imgElem.height);
@@ -36,70 +38,115 @@ const HistOfGradDemo: React.FC<HistOfGradDemoType> = ({
             canvasXElem.getContext('2d')?.fillRect(0, 0, canvasXElem.width, canvasXElem.height);
             canvasYElem.getContext('2d')?.fillRect(0, 0, canvasXElem.width, canvasXElem.height);
             canvasVElem.getContext('2d')?.fillRect(0, 0, canvasXElem.width, canvasXElem.height);
+            canvasNdlPltElem.getContext('2d')?.fillRect(0, 0, canvasNdlPltElem.width, canvasNdlPltElem.height);
         };
     })
 
     const applyImage = (imgUrl: any) => {
-        const canvasXElem = canvasX.current;
-        const canvasYElem = canvasY.current;
-        const canvasVElem = canvasV.current;
-
-        histogramBlocks(imgUrl);
-
         gradientImages(imgUrl).then((gradients: GradientsType) => {
             // @ts-ignore
-            displayGradients(canvasXElem, canvasYElem, canvasVElem, gradients);
-        })
+            displayGradients(canvasX.current, canvasY.current, canvasV.current, gradients);
+        });
 
-        const canvasHistElem = canvasHist.current;
         histogramAggregate(imgUrl).then((histogram: number[]) => {
-            console.log(histogram);
+            // @ts-ignore
+            displayHistogram(canvasHist.current, histogram);
+        });
 
-            const bins = histogram.length;
-            const labels: string[] = [];
-            for (let i = 0; i <= histogram.length; i++) labels.push(((180 / bins) * i).toString())
-            console.log(labels);
-            const chart =  new Chart(canvasHistElem?.getContext('2d'), {
-                type: 'bar',
-                data: {
-                  labels,
-                  datasets: [{
-                    label: 'Aggregated Histogram',
-                    data: histogram,
-                    backgroundColor: 'green',
-                  }]
-                },
-                options: {
-                    responsive: true,
-                  scales: {
-                    xAxes: [{
-                      display: false,
-                      barPercentage: 1.0,
-                      ticks: {
-                        max: 10,
-                      }
-                    }, {
-                      display: true,
-                      ticks: {
-                        autoSkip: false,
-                        max: 10,
-                      }
-                    }],
-                    yAxes: [{
-                      ticks: {
-                        beginAtZero: true
-                      }
-                    }]
-                  }
-                }
-              }); 
-        })
+        histogramBlocks(imgUrl).then((blocks: BlocksType) => {
+            // @ts-ignore
+            displayNeedlePlot(canvasNdlPlt.current, blocks);
+        });
     }
 
     const displayGradients = (cnvX: HTMLCanvasElement, cnvY: HTMLCanvasElement, cnvV: HTMLCanvasElement, gradients: GradientsType): void => {
         cnvX.getContext('2d')?.putImageData(gradients.x, 0, 0);
         cnvY.getContext('2d')?.putImageData(gradients.y, 0, 0);
         cnvV.getContext('2d')?.putImageData(gradients.v, 0, 0);
+    }
+
+    const displayNeedlePlot = (canvasNdlPlt: HTMLCanvasElement, blocks: BlocksType): void => {
+        const histograms: number[][][] = blocks.histogram;
+        const magnitudes: number[][] = blocks.blockMagnitudes;
+        console.log(magnitudes);
+        let maxV = 0, minV = 10, maxH = 0, minH = 10;
+        magnitudes.flat().forEach(v => {if (!isNaN(v)) {maxV = Math.max(maxV, v); minV = Math.min(minV, v);}});
+        histograms.flat().flat().forEach(h => {if (!isNaN(h)) {maxH = Math.max(maxH, h); minH = Math.min(minH, h);}});
+        console.log(minV, maxV);
+        console.log(minH, maxH);
+
+        for (let row = 0; row < histograms.length; row++) {
+            for (let col = 0; col < histograms[0].length; col++) {
+                const histogram: number[] = histograms[row][col];
+                const opacity: number = map(magnitudes[row][col], minV, maxV, 0, 255);
+                //console.log(`(${row}, ${col}): ${opacity}`);
+                for (let idx = 0; idx < histogram.length; idx++) {
+                    const lengthPct: number = map(histogram[idx], minH, maxH, 0, 1);
+                    drawNeedle(canvasNdlPlt, row, col, canvasNdlPlt.width / histograms.length, idx * 20, lengthPct, opacity);
+                }
+
+            }
+        }
+
+    }
+
+    const drawNeedle = (cnv: HTMLCanvasElement, blockRow: number, blockCol: number, blockSize: number, angleDeg: number, lengthPct: number, opacity: number): void => {
+        const length: number = lengthPct * blockSize;
+        console.log(lengthPct, length / blockSize);
+        const pt1 = {x: blockSize / 2 + (length / 2) * Math.cos(angleDeg * (Math.PI / 180)), y: blockSize / 2 + ( length / 2) * -1 * Math.sin(angleDeg * (Math.PI / 180))};
+        const pt2 = {x: blockSize / 2 + (length / 2) * -1 * Math.cos(angleDeg * (Math.PI / 180)), y: blockSize / 2 + ( length / 2) * Math.sin(angleDeg * (Math.PI / 180))};
+        const blockPos = {x: blockRow * blockSize, y: blockCol * blockSize};
+        const context = cnv.getContext('2d');
+        if (!context || !(context instanceof CanvasRenderingContext2D)) throw new Error('Failed to get 2D context');
+        const ctx: CanvasRenderingContext2D = context;
+
+        ctx.strokeStyle = `rgba(255, 255, 255, ${opacity})`;
+        ctx.beginPath();
+        ctx.moveTo(blockPos.x + pt1.x, blockPos.y + pt1.y);
+        ctx.lineTo(blockPos.x + pt2.x, blockPos.y + pt2.y);
+        ctx.stroke();
+    }
+
+
+    const displayHistogram = (cnvHist: HTMLCanvasElement, histogram: number[]): void => {
+        const bins = histogram.length;
+        const labels: string[] = [];
+        for (let i = 0; i <= histogram.length; i++) labels.push(((180 / bins) * i).toString())
+        console.log(labels);
+        const chart =  new Chart(cnvHist?.getContext('2d'), {
+            type: 'bar',
+            data: {
+                labels,
+                datasets: [{
+                    label: 'Aggregated Histogram',
+                    data: histogram,
+                    backgroundColor: 'green',
+                }]
+            },
+            options: {
+                responsive: true,
+                scales: {
+                    xAxes: [{
+                        display: false,
+                        barPercentage: 1.0,
+                        ticks: {
+                            max: 10,
+                        }
+                    }, {
+                        display: true,
+                        ticks: {
+                            autoSkip: false,
+                            max: 10,
+                        }
+                    }],
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        });
     }
 
     return (
@@ -125,6 +172,13 @@ const HistOfGradDemo: React.FC<HistOfGradDemoType> = ({
             <canvas
                 className="crisp-pixels mx-auto"
                 ref={canvasV}
+                width={imgWidth}
+                height={imgHeight}
+            />
+            <p className={labelColor}>Needle Plot</p>
+            <canvas
+                className="crisp-pixels mx-auto max-w-screen-md"
+                ref={canvasNdlPlt}
                 width={imgWidth}
                 height={imgHeight}
             />
--- src/modules/computerVision/histOfGrad/histOfGrad.tsx
+++ src/modules/computerVision/histOfGrad/histOfGrad.tsx
@@ -24,22 +24,47 @@ export async function histogramAggregate(img: any): Promise<number[]> {
   });
 }
 
-export async function histogramBlocks(img: any): Promise<number[][][]> {
+export type BlocksType = {
+  histogram: number[][][];
+  blockMagnitudes: number[][];
+};
+
+export async function histogramBlocks(img: any): Promise<BlocksType> {
   return Image.load(img).then((image: any) => {
-    const options = {
+    const normal = {
       cellSize: 8,
       blockSize: 2,
       blockStride: 1,
       bins: 9,
     };
+    const mediumSparse = {
+      cellSize: 12,
+      blockSize: 4,
+      blockStride: 2,
+      bins: 9,
+    };
+    const sparseBlocks = {
+      cellSize: 16,
+      blockSize: 4,
+      blockStride: 2,
+      bins: 9,
+    };
+    const options = mediumSparse;
     const descriptor = hog.extractHOG(image, options);
+
+    const intensities: { x: number[][], y: number[][] } = hog.gradients(image);
+    const intensitiesX: number[] = intensities.x.flat();
+    const intensitiesY: number[] = intensities.y.flat();
+    const intensitiesV: number[] = intensitiesX.map((num, idx) => Math.sqrt(Math.pow(num, 2) + Math.pow(intensitiesY[idx], 2)));
 
     const blockWidth = Math.floor((image.width / options.cellSize - options.blockSize) / options.blockStride) + 1;
     const blockHeight = Math.floor((image.height / options.cellSize - options.blockSize) / options.blockStride) + 1;
+    const blockPixels = options.cellSize * options.blockSize;
 
     const blockHistograms: number[][][] = [];
     for (let blockRow = 0; blockRow < blockWidth; blockRow += 1) {
       const row: number[][] = [];
+      const mRow: number[] = [];
       for (let blockCol = 0; blockCol < blockHeight; blockCol += 1) {
         const index = blockRow * blockWidth + blockCol;
         row.push(
@@ -51,9 +76,41 @@ export async function histogramBlocks(img: any): Promise<number[][][]> {
       }
       blockHistograms.push(row);
     }
-    console.log('block histograms: ', blockHistograms);
-    return blockHistograms;
+
+    // reshape intensitiesV to be [image width, image height]
+    const intensitiesV2D: number[][] = [];
+    for (let i = 0; i < image.width; i += 1) {
+      const row: number[] = [];
+      for (let j = 0; j < image.height; j += 1) {
+        const index = i * image.width + j;
+        row.push(intensitiesV[index]);
+      }
+      intensitiesV2D.push(row);
+    }
+
+    // for each block, calculate average of all pixels within block
+    const mBlocks: number[][] = [];
+    for (let blockRow = 0; blockRow < blockWidth; blockRow += 1) {
+      const mRow: number[] = [];
+      for (let blockCol = 0; blockCol < blockHeight; blockCol += 1) {
+        const index = blockRow * blockWidth + blockCol;
+        mRow.push(averageBlock(intensitiesV2D, blockRow, blockCol, blockPixels));
+      }
+      mBlocks.push(mRow);
+    }
+
+    return {histogram: blockHistograms, blockMagnitudes: mBlocks};
   });
+}
+
+function averageBlock(arr: number[][], row: number, col: number, length: number): number {
+  let sum = 0;
+  for (let i = 0; i < length; i++) {
+    for (let j = 0; j < length; j++) {
+      sum += arr[row+i][col+j];
+    }
+  }
+  return sum / (length * length);
 }
 
 export type GradientsType = {
@@ -63,7 +120,7 @@ export type GradientsType = {
   v: ImageData;
 };
 
-const map = (x: number, min1: number, max1: number, min2: number, max2: number): number => (x - min1) * (max2 - min2) / (max1 - min1) + min2;
+export const map = (x: number, min1: number, max1: number, min2: number, max2: number): number => (x - min1) * (max2 - min2) / (max1 - min1) + min2;
 
 export async function gradientImages(img: any): Promise<GradientsType> {
   return Image.load(img).then((image: any) => {
