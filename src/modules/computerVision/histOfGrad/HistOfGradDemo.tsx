801e1444e38580b17ed3875c551d958ed18e3991
stanleykywu <stanleykywu@gmail.com>
2021-04-03 12:44:47 -0400

    implemented simple buttons for hog tabs

--- src/modules/computerVision/histOfGrad/HistOfGradDemo.tsx
+++ src/modules/computerVision/histOfGrad/HistOfGradDemo.tsx
@@ -9,178 +9,281 @@ type HistOfGradDemoType = {
     imgUrl: string;
 }
 
+type HogTabType = {
+  labelColor: string;
+  imgUrl: string;
+  width: number;
+  height: number;
+}
+
+const SobelTab: React.FC<HogTabType> = ({
+  labelColor,
+  imgUrl,
+  width,
+  height,
+}) => {
+  const canvasX = useRef<HTMLCanvasElement>(null);
+  const canvasY = useRef<HTMLCanvasElement>(null);
+  const canvasXElem = canvasX.current;
+  const canvasYElem = canvasY.current;
+
+  useLayoutEffect(() => {
+    const canvasXElem = canvasX.current;
+    const canvasYElem = canvasY.current;
+    if (!(canvasXElem && canvasYElem)) return;
+
+    gradientImages(imgUrl).then((gradients: GradientsType) => {
+      // @ts-ignore
+      displayGradients(canvasX.current, canvasY.current, gradients);
+    })
+  
+    const displayGradients = (cnvX: HTMLCanvasElement, cnvY: HTMLCanvasElement, gradients: GradientsType): void => {
+      cnvX.getContext('2d')?.putImageData(gradients.x, 0, 0);
+      cnvY.getContext('2d')?.putImageData(gradients.y, 0, 0);
+    }
+  })
+  
+  return (
+    <div>
+        <p className={labelColor}>Horizontal Sobel</p>
+        <canvas
+            className="crisp-pixels mx-auto"
+            ref={canvasX}
+            width={width}
+            height={height}
+        />
+        <p className={labelColor}>Vertical Sobel</p>
+        <canvas
+            className="crisp-pixels mx-auto"
+            ref={canvasY}
+            width={width}
+            height={height}
+        />
+    </div>
+  )
+}
+
+const CombinedSobelTab: React.FC<HogTabType> = ({
+  labelColor,
+  imgUrl,
+  width,
+  height,
+}) => {
+  const canvasX = useRef<HTMLCanvasElement>(null);
+  const canvasY = useRef<HTMLCanvasElement>(null);
+  const canvasV = useRef<HTMLCanvasElement>(null);
+  const canvasXElem = canvasX.current;
+  const canvasYElem = canvasY.current;
+
+  useLayoutEffect(() => {
+    const canvasXElem = canvasX.current;
+    const canvasYElem = canvasY.current;
+    const canvasVElem = canvasV.current;
+    if (!(canvasXElem && canvasYElem && canvasVElem)) return;
+
+    gradientImages(imgUrl).then((gradients: GradientsType) => {
+      // @ts-ignore
+      displayGradients(canvasX.current, canvasY.current, canvasV.current, gradients);
+    })
+  
+    const displayGradients = (cnvX: HTMLCanvasElement, cnvY: HTMLCanvasElement, cnvV: HTMLCanvasElement, gradients: GradientsType): void => {
+      cnvX.getContext('2d')?.putImageData(gradients.x, 0, 0);
+      cnvY.getContext('2d')?.putImageData(gradients.y, 0, 0);
+      cnvV.getContext('2d')?.putImageData(gradients.v, 0, 0);
+    }
+  })
+  
+  return (
+    <div className="flex justify-evenly">
+      <div>
+        <p className={labelColor}>Horizontal Sobel</p>
+        <canvas
+            className="crisp-pixels mx-auto"
+            ref={canvasX}
+            width={width}
+            height={height}
+        />
+        <p className={labelColor}>Vertical Sobel</p>
+        <canvas
+            className="crisp-pixels mx-auto"
+            ref={canvasY}
+            width={width}
+            height={height}
+        />
+      </div>
+      <div className="my-auto">
+        <p className={labelColor}>Combined Sobel</p>
+        <canvas
+            className="crisp-pixels mx-auto"
+            ref={canvasV}
+            width={width}
+            height={height}
+        />
+      </div>
+    </div>
+  )
+}
+
+const NeedleHistogramTab: React.FC<HogTabType> = ({
+  labelColor,
+  imgUrl,
+  width,
+  height,
+}) => {
+  const canvasHist = useRef<HTMLCanvasElement>(null);
+  const canvasNdlPlt = useRef<HTMLCanvasElement>(null);
+
+  useLayoutEffect(() => {
+    const canvasHistElem = canvasHist.current;
+    const canvasNdlPltElem = canvasNdlPlt.current;
+    if (!(canvasHistElem && canvasNdlPltElem)) return;
+
+    gradientImages(imgUrl).then(() => {
+      histogramBlocks(imgUrl).then((blocks: BlocksType) => {
+      // @ts-ignore
+      displayNeedlePlot(canvasNdlPlt.current, blocks);
+      });
+
+      histogramAggregate(imgUrl).then((histogram: number[]) => {
+          // @ts-ignore
+          displayHistogram(canvasHist.current, histogram);
+      });
+    })
+  
+    const displayNeedlePlot = (canvasNdlPlt: HTMLCanvasElement, blocks: BlocksType): void => {
+      canvasNdlPlt.getContext('2d')?.fillRect(0, 0, width, height);
+      const histograms: number[][][] = blocks.histogram;
+      const magnitudes: number[][] = blocks.blockMagnitudes;
+      let maxV = 0, minV = 10, maxH = 0, minH = 10;
+      magnitudes.flat().forEach(v => {if (!isNaN(v)) {maxV = Math.max(maxV, v); minV = Math.min(minV, v);}});
+      histograms.flat().flat().forEach(h => {if (!isNaN(h)) {maxH = Math.max(maxH, h); minH = Math.min(minH, h);}});
+      for (let row = 0; row < histograms.length; row++) {
+          for (let col = 0; col < histograms[0].length; col++) {
+              const histogram: number[] = histograms[row][col];
+              const opacity: number = map(magnitudes[row][col], minV, maxV, 25, 255);
+              for (let idx = 0; idx < histogram.length; idx++) {
+                  const lengthPct: number = map(histogram[idx], minH, maxH, 0, 1);
+                  drawNeedle(canvasNdlPlt, row, col, canvasNdlPlt.width / histograms[0].length, idx * 20, lengthPct, opacity);
+              }
+
+          }
+      }
+
+  }
+
+  const drawNeedle = (cnv: HTMLCanvasElement, blockRow: number, blockCol: number, blockSize: number, angleDeg: number, lengthPct: number, opacity: number): void => {
+      const length: number = lengthPct * blockSize;
+      const pt1 = {x: blockSize / 2 + (length / 2) * Math.cos(angleDeg * (Math.PI / 180)), y: blockSize / 2 + ( length / 2) * -1 * Math.sin(angleDeg * (Math.PI / 180))};
+      const pt2 = {x: blockSize / 2 + (length / 2) * -1 * Math.cos(angleDeg * (Math.PI / 180)), y: blockSize / 2 + ( length / 2) * Math.sin(angleDeg * (Math.PI / 180))};
+      const blockPos = {x: blockCol * blockSize, y: blockRow * blockSize};
+      const context = cnv.getContext('2d');
+      if (!context || !(context instanceof CanvasRenderingContext2D)) throw new Error('Failed to get 2D context');
+      const ctx: CanvasRenderingContext2D = context;
+
+      ctx.strokeStyle = `rgba(${opacity}, ${opacity}, ${opacity}, ${opacity / 255})`;
+      ctx.beginPath();
+      ctx.moveTo(blockPos.x + pt1.x, blockPos.y + pt1.y);
+      ctx.lineTo(blockPos.x + pt2.x, blockPos.y + pt2.y);
+      ctx.stroke();
+  }
+
+
+  const displayHistogram = (cnvHist: HTMLCanvasElement, histogram: number[]): void => {
+      const bins = histogram.length;
+      const labels: string[] = [];
+      for (let i = 0; i <= histogram.length; i++) labels.push(((180 / bins) * i).toString())
+      const chart =  new Chart(cnvHist?.getContext('2d'), {
+          type: 'bar',
+          data: {
+              labels,
+              datasets: [{
+                  label: 'Aggregated Histogram',
+                  data: histogram,
+                  backgroundColor: 'green',
+              }]
+          },
+          options: {
+              responsive: true,
+              scales: {
+                  xAxes: [{
+                      display: false,
+                      barPercentage: 1.0,
+                      ticks: {
+                          max: 10,
+                      }
+                  }, {
+                      display: true,
+                      ticks: {
+                          autoSkip: false,
+                          max: 10,
+                      }
+                  }],
+                  yAxes: [{
+                      ticks: {
+                          beginAtZero: true
+                      }
+                  }]
+              }
+          }
+      });
+  }
+  })
+  
+  return (
+    <div className="flex justify-evenly">
+      <div>
+        <p className={labelColor}>Needle Plot</p>
+        <canvas
+            className="crisp-pixels mx-auto max-w-screen-md"
+            ref={canvasNdlPlt}
+            width={width}
+            height={height}
+        />
+      </div>
+      <div>
+        <p className={labelColor}>Aggregated Histogram</p>
+        <canvas
+            className="crisp-pixels mx-auto max-w-screen-md"
+            ref={canvasHist}
+            width={width}
+            height={height}
+        />
+      </div>
+    </div>
+  )
+}
+
+
 const HistOfGradDemo: React.FC<HistOfGradDemoType> = ({
   labelColor,
-    imgUrl,
+  imgUrl,
 }) => {
-    const canvasX = useRef<HTMLCanvasElement>(null);
-    const canvasY = useRef<HTMLCanvasElement>(null);
-    const canvasV = useRef<HTMLCanvasElement>(null);
-    const canvasHist = useRef<HTMLCanvasElement>(null);
-    const canvasNdlPlt = useRef<HTMLCanvasElement>(null);
     const imgRef = useRef<HTMLImageElement>(null);
-    const [imgWidth, setImgWidth] = useState<number>();
-    const [imgHeight, setImgHeight] = useState<number>();
+    const [imgWidth, setImgWidth] = useState<number>(0);
+    const [imgHeight, setImgHeight] = useState<number>(0);
+    const [tab, setTab] = useState<number>(0);
 
     useLayoutEffect(() => {
-        const canvasXElem = canvasX.current;
-        const canvasYElem = canvasY.current;
-        const canvasVElem = canvasV.current;
-        const canvasHistElem = canvasHist.current;
-        const canvasNdlPltElem = canvasNdlPlt.current;
-        const imgElem = imgRef.current;
-        if (!(canvasXElem && canvasYElem && canvasVElem && imgElem && canvasHistElem && canvasNdlPltElem)) return;
+      const imgElem = imgRef.current;
 
-        imgElem.onload = () => {
-            setImgHeight(imgElem.height);
-            setImgWidth(imgElem.width);
-            canvasXElem.getContext('2d')?.fillRect(0, 0, canvasXElem.width, canvasXElem.height);
-            canvasYElem.getContext('2d')?.fillRect(0, 0, canvasXElem.width, canvasXElem.height);
-            canvasVElem.getContext('2d')?.fillRect(0, 0, canvasXElem.width, canvasXElem.height);
-            canvasNdlPltElem.getContext('2d')?.fillRect(0, 0, canvasNdlPltElem.width, canvasNdlPltElem.height);
-        };
+      if (!imgElem) return;
+      imgElem.onload = () => {
+        setImgHeight(imgElem.height);
+        setImgWidth(imgElem.width);
+      };
     })
-
-    const applyImage = (imgUrl: any) => {
-        gradientImages(imgUrl).then((gradients: GradientsType) => {
-            // @ts-ignore
-            displayGradients(canvasX.current, canvasY.current, canvasV.current, gradients);
-        });
-
-        histogramBlocks(imgUrl).then((blocks: BlocksType) => {
-            // @ts-ignore
-            displayNeedlePlot(canvasNdlPlt.current, blocks);
-        });
-
-        histogramAggregate(imgUrl).then((histogram: number[]) => {
-            // @ts-ignore
-            displayHistogram(canvasHist.current, histogram);
-        });
-    }
-
-    const displayGradients = (cnvX: HTMLCanvasElement, cnvY: HTMLCanvasElement, cnvV: HTMLCanvasElement, gradients: GradientsType): void => {
-        cnvX.getContext('2d')?.putImageData(gradients.x, 0, 0);
-        cnvY.getContext('2d')?.putImageData(gradients.y, 0, 0);
-        cnvV.getContext('2d')?.putImageData(gradients.v, 0, 0);
-    }
-
-    const displayNeedlePlot = (canvasNdlPlt: HTMLCanvasElement, blocks: BlocksType): void => {
-        const histograms: number[][][] = blocks.histogram;
-        const magnitudes: number[][] = blocks.blockMagnitudes;
-        let maxV = 0, minV = 10, maxH = 0, minH = 10;
-        magnitudes.flat().forEach(v => {if (!isNaN(v)) {maxV = Math.max(maxV, v); minV = Math.min(minV, v);}});
-        histograms.flat().flat().forEach(h => {if (!isNaN(h)) {maxH = Math.max(maxH, h); minH = Math.min(minH, h);}});
-        for (let row = 0; row < histograms.length; row++) {
-            for (let col = 0; col < histograms[0].length; col++) {
-                const histogram: number[] = histograms[row][col];
-                const opacity: number = map(magnitudes[row][col], minV, maxV, 25, 255);
-                for (let idx = 0; idx < histogram.length; idx++) {
-                    const lengthPct: number = map(histogram[idx], minH, maxH, 0, 1);
-                    drawNeedle(canvasNdlPlt, row, col, canvasNdlPlt.width / histograms[0].length, idx * 20, lengthPct, opacity);
-                }
-
-            }
-        }
-
-    }
-
-    const drawNeedle = (cnv: HTMLCanvasElement, blockRow: number, blockCol: number, blockSize: number, angleDeg: number, lengthPct: number, opacity: number): void => {
-        const length: number = lengthPct * blockSize;
-        const pt1 = {x: blockSize / 2 + (length / 2) * Math.cos(angleDeg * (Math.PI / 180)), y: blockSize / 2 + ( length / 2) * -1 * Math.sin(angleDeg * (Math.PI / 180))};
-        const pt2 = {x: blockSize / 2 + (length / 2) * -1 * Math.cos(angleDeg * (Math.PI / 180)), y: blockSize / 2 + ( length / 2) * Math.sin(angleDeg * (Math.PI / 180))};
-        const blockPos = {x: blockCol * blockSize, y: blockRow * blockSize};
-        const context = cnv.getContext('2d');
-        if (!context || !(context instanceof CanvasRenderingContext2D)) throw new Error('Failed to get 2D context');
-        const ctx: CanvasRenderingContext2D = context;
-
-        ctx.strokeStyle = `rgba(${opacity}, ${opacity}, ${opacity}, ${opacity / 255})`;
-        ctx.beginPath();
-        ctx.moveTo(blockPos.x + pt1.x, blockPos.y + pt1.y);
-        ctx.lineTo(blockPos.x + pt2.x, blockPos.y + pt2.y);
-        ctx.stroke();
-    }
-
-
-    const displayHistogram = (cnvHist: HTMLCanvasElement, histogram: number[]): void => {
-        const bins = histogram.length;
-        const labels: string[] = [];
-        for (let i = 0; i <= histogram.length; i++) labels.push(((180 / bins) * i).toString())
-        const chart =  new Chart(cnvHist?.getContext('2d'), {
-            type: 'bar',
-            data: {
-                labels,
-                datasets: [{
-                    label: 'Aggregated Histogram',
-                    data: histogram,
-                    backgroundColor: 'green',
-                }]
-            },
-            options: {
-                responsive: true,
-                scales: {
-                    xAxes: [{
-                        display: false,
-                        barPercentage: 1.0,
-                        ticks: {
-                            max: 10,
-                        }
-                    }, {
-                        display: true,
-                        ticks: {
-                            autoSkip: false,
-                            max: 10,
-                        }
-                    }],
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero: true
-                        }
-                    }]
-                }
-            }
-        });
-    }
 
     return (
         <div>
-            <img ref={imgRef} src={imgUrl} alt="input" className="mx-auto" />
-            <button onClick={() => applyImage(imgUrl)} className={`btn ${labelColor}`}>Apply Gradients</button>
-            <br/>
-            <p className={labelColor}>Horizontal Sobel</p>
-            <canvas
-                className="crisp-pixels mx-auto"
-                ref={canvasX}
-                width={imgWidth}
-                height={imgHeight}
-            />
-            <p className={labelColor}>Vertical Sobel</p>
-            <canvas
-                className="crisp-pixels mx-auto"
-                ref={canvasY}
-                width={imgWidth}
-                height={imgHeight}
-            />
-            <p className={labelColor}>Combined Sobel</p>
-            <canvas
-                className="crisp-pixels mx-auto"
-                ref={canvasV}
-                width={imgWidth}
-                height={imgHeight}
-            />
-            <p className={labelColor}>Needle Plot</p>
-            <canvas
-                className="crisp-pixels mx-auto max-w-screen-md"
-                ref={canvasNdlPlt}
-                width={imgWidth}
-                height={imgHeight}
-            />
-            <p className={labelColor}>Aggregated Histogram</p>
-            <canvas
-                className="crisp-pixels mx-auto max-w-screen-md"
-                ref={canvasHist}
-                width={imgWidth}
-                height={imgHeight}
-            />
+          <img ref={imgRef} src={imgUrl} alt="image" className="hidden"></img>
+          <div>
+            <button className="basic-button" onClick={() => setTab(0)}>1</button>
+            <button className="basic-button" onClick={() => setTab(1)}>2</button>
+            <button className="basic-button" onClick={() => setTab(2)}>3</button>
+          </div>
+            {tab === 0 && <SobelTab labelColor={labelColor} imgUrl={imgUrl} width={imgWidth} height={imgHeight} />}
+            {tab === 1 && <CombinedSobelTab labelColor={labelColor} imgUrl={imgUrl} width={imgWidth} height={imgHeight} />}
+            {tab === 2 && <NeedleHistogramTab labelColor={labelColor} imgUrl={imgUrl} width={imgWidth} height={imgHeight} />}
         </div>
     );
 }
