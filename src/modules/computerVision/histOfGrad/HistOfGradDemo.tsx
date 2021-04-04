2bca92b3329309db0dbdfa0b7e47ba0501baa80d
stanleykywu <stanleykywu@gmail.com>
2021-04-04 14:35:06 -0400

    changed layout for tabs and enlarged images

--- src/modules/computerVision/histOfGrad/HistOfGradDemo.tsx
+++ src/modules/computerVision/histOfGrad/HistOfGradDemo.tsx
@@ -73,23 +73,62 @@ const CombinedSobelTab: React.FC<HogTabType> = ({
 }) => {
   const canvasX = useRef<HTMLCanvasElement>(null);
   const canvasY = useRef<HTMLCanvasElement>(null);
-  const canvasV = useRef<HTMLCanvasElement>(null);
+  const canvasNdlPlt = useRef<HTMLCanvasElement>(null);
 
   useLayoutEffect(() => {
     const canvasXElem = canvasX.current;
     const canvasYElem = canvasY.current;
-    const canvasVElem = canvasV.current;
-    if (!(canvasXElem && canvasYElem && canvasVElem)) return;
+    const canvasNdlPltElem = canvasNdlPlt.current;
+    if (!(canvasXElem && canvasYElem && canvasNdlPltElem)) return;
 
     gradientImages(imgUrl).then((gradients: GradientsType) => {
-      // @ts-ignore
-      displayGradients(canvasX.current, canvasY.current, canvasV.current, gradients);
+      histogramBlocks(imgUrl).then((blocks: BlocksType) => {
+          // @ts-ignore
+          displayNeedlePlot(canvasNdlPlt.current, blocks);
+          // @ts-ignore
+          displayGradients(canvasX.current, canvasY.current, gradients);
+      });
     })
   
-    const displayGradients = (cnvX: HTMLCanvasElement, cnvY: HTMLCanvasElement, cnvV: HTMLCanvasElement, gradients: GradientsType): void => {
+    const displayGradients = (cnvX: HTMLCanvasElement, cnvY: HTMLCanvasElement, gradients: GradientsType): void => {
       cnvX.getContext('2d')?.putImageData(gradients.x, 0, 0);
       cnvY.getContext('2d')?.putImageData(gradients.y, 0, 0);
-      cnvV.getContext('2d')?.putImageData(gradients.v, 0, 0);
+    }
+
+
+    const displayNeedlePlot = (canvasNdlPlt: HTMLCanvasElement, blocks: BlocksType): void => {
+      canvasNdlPlt.getContext('2d')?.fillRect(0, 0, canvasNdlPlt.width, canvasNdlPlt.height);
+      const histograms: number[][][] = blocks.histogram;
+      const magnitudes: number[][] = blocks.blockMagnitudes;
+      let maxV = 0, minV = 10, maxH = 0, minH = 10;
+      magnitudes.flat().forEach(v => {if (!isNaN(v)) {maxV = Math.max(maxV, v); minV = Math.min(minV, v);}});
+      histograms.flat().flat().forEach(h => {if (!isNaN(h)) {maxH = Math.max(maxH, h); minH = Math.min(minH, h);}});
+      for (let row = 0; row < histograms.length; row++) {
+          for (let col = 0; col < histograms[0].length; col++) {
+              const histogram: number[] = histograms[row][col];
+              const opacity: number = map(magnitudes[row][col], minV, maxV, 25, 255);
+              for (let idx = 0; idx < histogram.length; idx++) {
+                  const lengthPct: number = map(histogram[idx], minH, maxH, 0, 1);
+                  drawNeedle(canvasNdlPlt, row, col, canvasNdlPlt.width / histograms[0].length, idx * 20, lengthPct, opacity);
+              }
+          }
+      }
+  }
+
+  const drawNeedle = (cnv: HTMLCanvasElement, blockRow: number, blockCol: number, blockSize: number, angleDeg: number, lengthPct: number, opacity: number): void => {
+      const length: number = lengthPct * blockSize;
+      const pt1 = {x: blockSize / 2 + (length / 2) * Math.cos(angleDeg * (Math.PI / 180)), y: blockSize / 2 + ( length / 2) * -1 * Math.sin(angleDeg * (Math.PI / 180))};
+      const pt2 = {x: blockSize / 2 + (length / 2) * -1 * Math.cos(angleDeg * (Math.PI / 180)), y: blockSize / 2 + ( length / 2) * Math.sin(angleDeg * (Math.PI / 180))};
+      const blockPos = {x: blockCol * blockSize, y: blockRow * blockSize};
+      const context = cnv.getContext('2d');
+      if (!context || !(context instanceof CanvasRenderingContext2D)) throw new Error('Failed to get 2D context');
+      const ctx: CanvasRenderingContext2D = context;
+
+      ctx.strokeStyle = `rgba(${opacity}, ${opacity}, ${opacity}, ${opacity / 255})`;
+      ctx.beginPath();
+      ctx.moveTo(blockPos.x + pt1.x, blockPos.y + pt1.y);
+      ctx.lineTo(blockPos.x + pt2.x, blockPos.y + pt2.y);
+      ctx.stroke();
     }
   })
   
@@ -112,12 +151,12 @@ const CombinedSobelTab: React.FC<HogTabType> = ({
         />
       </div>
       <div className="my-auto">
-        <p className={labelColor}>Combined Sobel</p>
+        <p className={labelColor}>Needle Plot</p>
         <canvas
             className="crisp-pixels mx-auto"
-            ref={canvasV}
-            width={width}
-            height={height}
+            ref={canvasNdlPlt}
+            width={width * 1.5}
+            height={height * 1.5}
         />
       </div>
     </div>
@@ -149,7 +188,8 @@ const SobelNeedleTab: React.FC<HogTabType> = (
         })
 
         const displayGradients = ( cnvV: HTMLCanvasElement, gradients: GradientsType): void => {
-            cnvV.getContext('2d')?.putImageData(gradients.v, 0, 0);
+            // cnvV.getContext('2d')?.putImageData(gradients.v, 0, 0);
+            createImageBitmap(gradients.v).then(img => cnvV.getContext('2d')?.drawImage(img, 0, 0, width, height));
         }
 
         const displayNeedlePlot = (canvasNdlPlt: HTMLCanvasElement, blocks: BlocksType): void => {
@@ -167,10 +207,8 @@ const SobelNeedleTab: React.FC<HogTabType> = (
                         const lengthPct: number = map(histogram[idx], minH, maxH, 0, 1);
                         drawNeedle(canvasNdlPlt, row, col, canvasNdlPlt.width / histograms[0].length, idx * 20, lengthPct, opacity);
                     }
-
                 }
             }
-
         }
 
         const drawNeedle = (cnv: HTMLCanvasElement, blockRow: number, blockCol: number, blockSize: number, angleDeg: number, lengthPct: number, opacity: number): void => {
@@ -296,21 +334,23 @@ const NeedleHistogramTab: React.FC<HogTabType> = ({
               responsive: true,
               scales: {
                   xAxes: [{
-                      display: false,
-                      barPercentage: 1.0,
-                      ticks: {
-                          max: 10,
-                      }
-                  }, {
                       display: true,
                       ticks: {
                           autoSkip: false,
                           max: 10,
+                      },
+                      scaleLabel: {
+                        display: true,
+                        labelString: 'Angle'
                       }
                   }],
                   yAxes: [{
                       ticks: {
                           beginAtZero: true
+                      },
+                      scaleLabel: {
+                        display: true,
+                        labelString: 'Total Magnitude'
                       }
                   }]
               }
@@ -374,8 +414,8 @@ const HistOfGradDemo: React.FC<HistOfGradDemoType> = ({
           </div>
             {tab === 0 && <SobelTab labelColor={labelColor} imgUrl={imgUrl} width={imgWidth} height={imgHeight} />}
             {tab === 1 && <CombinedSobelTab labelColor={labelColor} imgUrl={imgUrl} width={imgWidth} height={imgHeight} />}
-            {tab === 2 && <SobelNeedleTab labelColor={labelColor} imgUrl={imgUrl} width={imgWidth} height={imgHeight} />}
-            {tab === 3 && <NeedleHistogramTab labelColor={labelColor} imgUrl={imgUrl} width={imgWidth} height={imgHeight} />}
+            {tab === 2 && <SobelNeedleTab labelColor={labelColor} imgUrl={imgUrl} width={imgWidth * 1.5} height={imgHeight * 1.5} />}
+            {tab === 3 && <NeedleHistogramTab labelColor={labelColor} imgUrl={imgUrl} width={imgWidth * 1.5} height={imgHeight * 1.5} />}
         </div>
     );
 }
