7f5be8acd6af07c18ece1f5fdf58caadf7d587d8
jciolfi <johnc2744@gmail.com>
2021-11-07 21:02:12 -0500

    remove extra "generate kernel" button styling change

--- src/modules/computerVision/gaussianBlur/GaussianBlurDemo.tsx
+++ src/modules/computerVision/gaussianBlur/GaussianBlurDemo.tsx
@@ -68,7 +68,7 @@ const KernelConfig: React.FC<KernelConfigType> = ({ onConfig, labelColor }) => {
       <div className="p-6">
         <button
           type="button"
-          className={`basic-button ${window.innerWidth <= 470 ? 'mt-12' : ''}`}
+          className="basic-button"
           disabled={invalidConfig}
           onClick={() => onConfig(kernelSize, sigma)}
         >
