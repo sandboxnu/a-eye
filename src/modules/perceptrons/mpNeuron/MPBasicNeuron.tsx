48af88260ad385d9a4b1eff62291b398969a08b5
Connor Barker <connorlbark@gmail.com>
2021-11-14 15:39:59 -0500

    make bad labels

--- src/modules/perceptrons/mpNeuron/MPBasicNeuron.tsx
+++ src/modules/perceptrons/mpNeuron/MPBasicNeuron.tsx
@@ -86,16 +86,15 @@ export const MPBasicNeuron: React.FC<MPBasicNeuronType> = ({
 
   const Input = ({ inpt, weight, idx }) => (
     <div
-      className={`flex items-center cursor-pointer ${
-        hideInputs ? "pb-2.5 pt-16" : ""
-      }`}
+      className={`flex items-center cursor-pointer ${hideInputs ? "pb-2.5 pt-16" : ""
+        }`}
       key={idx}
     >
       {!hideInputs &&
         (setInputs ? (
           <div className="m-1">
             <input
-              className="number-input w-20 h-10 border-2 border-pink-700"
+              className="number-input w-20 h-10 border-2 border-teal-700"
               type="number"
               value={inpt}
               onChange={(e) => changeVal(e, idx)}
@@ -127,71 +126,85 @@ export const MPBasicNeuron: React.FC<MPBasicNeuronType> = ({
 
   return (
     <div className="m-2 flex flex-col items-center justify-center">
-      <div className="flex items-center">
-        <div className="flex flex-col h-36">
-          {inputs &&
-            weights &&
-            zip(inputs, weights).map(([input, weight], index) => (
-              <Input inpt={input} weight={weight} idx={index} key={index} />
-            ))}
-          {typeof bias === "number" && (
-            <div className="flex items-center self-end">
-              <p className={labelColor}>bias</p>
-              <div
-                className="font-bold rounded-full w-12 h-12 bg-pink-700 m-1
+      <div>
+
+        <div className="flex flex-row justify-start w-full">
+          {/* I hate this */}
+          <p className={`ml-4  ${labelColor}`}>Input</p>
+          <p className={`ml-16  ${labelColor}`}>Weights</p>
+          <p className='ml-8 mr-6' />
+          <p className={`ml-24 ${labelColor}`}>Sum</p>
+          <p className={`ml-6 ${labelColor}`}>Threshold</p>
+          <p className={`ml-12 ${labelColor}`}>Result</p>
+        </div>
+        
+        <div className="flex items-center">
+          <div className="flex flex-col h-36">
+            {inputs &&
+              weights &&
+              zip(inputs, weights).map(([input, weight], index) => (
+                <Input inpt={input} weight={weight} idx={index} key={index} />
+              ))}
+            {typeof bias === "number" && (
+              <div className="flex items-center self-end">
+                <p className={labelColor}>bias</p>
+                <div
+                  className="font-bold rounded-full w-12 h-12 bg-pink-700 m-1
                                         flex items-center justify-center text-white"
-              >
-                {bias.toFixed(1)}
+                >
+                  {bias.toFixed(1)}
+                </div>
               </div>
+            )}
+          </div>
+          <InputLines
+            numInpts={(typeof bias === "number" ? 1 : 0) + inputs.length}
+            transformY={hideInputs ? 2 : 1}
+          />
+
+          {typeof finalInputSum === "number" && (
+            <div
+              className="rounded-full w-20 h-20 bg-brightOrange
+                flex items-center justify-center"
+            >
+              {finalInputSum.toFixed(2)}
             </div>
           )}
-        </div>
-        <InputLines
-          numInpts={(typeof bias === "number" ? 1 : 0) + inputs.length}
-          transformY={hideInputs ? 2 : 1}
-        />
-        {typeof finalInputSum === "number" && (
+          <div className="w-2 h-1 bg-navy" />
+          <ControlledThresholdFunc
+            labelColor={labelColor}
+            threshold={threshold}
+            setThreshold={setThreshold}
+            isGreater={isGreater}
+            setIsGreater={setIsGreater}
+          />
+          <div className="w-16 h-1 bg-navy" />
           <div
-            className="rounded-full w-20 h-20 bg-brightOrange
-                flex items-center justify-center"
+            className="rounded-full w-12 h-12 font-bold bg-moduleTeal flex items-center justify-center"
+            style={{
+              backgroundColor: finalOutput === 1 ? OUTPT_CLR : "white",
+              border: finalOutput === 1 ? "none" : `2px solid ${OUTPT_CLR}`,
+            }}
           >
-            {finalInputSum.toFixed(2)}
+            {finalOutput}
+          </div>
+        </div>
+        {canAddInputs && setInputs && (
+          <div>
+            <RemoveCircle
+              className="icon-button"
+              fontSize="large"
+              onClick={removeInput}
+            />
+            <p className="inline m-2 text-white">{inputs.length} inputs</p>
+            <AddCircle
+              className="icon-button"
+              fontSize="large"
+              onClick={addInput}
+            />
           </div>
         )}
-        <div className="w-2 h-1 bg-navy" />
-        <ControlledThresholdFunc
-          labelColor={labelColor}
-          threshold={threshold}
-          setThreshold={setThreshold}
-          isGreater={isGreater}
-          setIsGreater={setIsGreater}
-        />
-        <div className="w-16 h-1 bg-navy" />
-        <div
-          className="rounded-full w-12 h-12 font-bold bg-moduleTeal flex items-center justify-center"
-          style={{
-            backgroundColor: finalOutput === 1 ? OUTPT_CLR : "white",
-            border: finalOutput === 1 ? "none" : `2px solid ${OUTPT_CLR}`,
-          }}
-        >
-          {finalOutput}
-        </div>
       </div>
-      {canAddInputs && setInputs && (
-        <div>
-          <RemoveCircle
-            className="icon-button"
-            fontSize="large"
-            onClick={removeInput}
-          />
-          <p className="inline m-2 text-white">{inputs.length} inputs</p>
-          <AddCircle
-            className="icon-button"
-            fontSize="large"
-            onClick={addInput}
-          />
-        </div>
-      )}
     </div>
   );
 };
