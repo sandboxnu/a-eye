c84153b5d85bba518d5ed9512e8f8bde24089b34
John Ciolfi <jciolfi@chewy.com>
2021-11-16 19:20:44 -0500

    updating module color styling

--- src/modules/computerVision/ComputerVisionList.tsx
+++ src/modules/computerVision/ComputerVisionList.tsx
@@ -12,31 +12,31 @@ const botStyle: CSS.Properties = {
 
 export default function ComputerVisionList() {
     return (
-        <div
-            className="flex items-center bg-cover w-screen h-auto"
-            style={botStyle}
-        >
-            <ul className="list-none">
-                {descriptions.modules
-                    .filter(module => module.parentModule === "Computer Vision")
-                    .map(module => (
-                        <a href={`/modules/${module.path}`}>
-                  <li className="flex justify-center my-48">
-                    <ModuleIntro
-                      title={module.title}
-                      body={module.body}
-                      bgColor={module.bgColor}
-                      key={module.number.toString()}
-                      textColor={module.textColor}
-                      margin={module.margin}
-                      imgSrc={module.imgSrc}
-                      active={module.active}
-                    />
-                  </li>
-                </a>
-                    ))
-                }
-            </ul>
-        </div>
+      <div
+        className="flex items-center bg-cover w-screen h-auto"
+        style={botStyle}
+      >
+        <ul className="list-none">
+          {descriptions.modules
+            .filter(module => module.parentModule === "Computer Vision")
+            .map(module => (
+              <a href={`/modules/${module.path}`}>
+                <li className="flex justify-center my-48">
+                  <ModuleIntro
+                    title={module.title}
+                    body={module.body}
+                    bgColor={module.bgColor}
+                    key={module.number.toString()}
+                    textColor={module.textColor}
+                    margin={module.margin}
+                    imgSrc={module.imgSrc}
+                    active={module.active}
+                  />
+                </li>
+              </a>
+            ))
+          }
+        </ul>
+      </div>
     )
 }
