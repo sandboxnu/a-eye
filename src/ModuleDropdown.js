150991aba0db760108ff6a2a357083c1e5dc5a3f
John Ciolfi <johnc2744@gmail.com>
2021-12-12 18:57:46 -0500

    add alerts, set cookie length to 4 months

--- src/ModuleDropdown.js
+++ src/ModuleDropdown.js
@@ -4,34 +4,43 @@ import { useCookies } from 'react-cookie';
 import descriptions from './media/modules/module_descriptions.json';
 
 export default function ModuleDropdown() {
-  const [cookies, setCookies] = useCookies(['isAuthenticated']);
+  const [cookies, setCookies] = useCookies(['aeye_authenticated']);
   const [hover, setHover] = useState({ button: false, dropdown: false });
 
   return (
-    <div className="">
-      <button onMouseOver={() => setHover({ button: cookies.isAuthenticated === "true", dropdown: hover.dropdown })}
-              onMouseOut={() => setHover({ button: false, dropdown: hover.dropdown })}
-              onClick={() => {
-                  if (cookies.isAuthenticated !== 'true') {
-                      const response = prompt('Enter The Modules Password');
-                      setCookies('isAuthenticated', response === 'SandboxNU', {path: '/'});
-                  }
-              }}>
-          <p className={`text-xs ${cookies.isAuthenticated === 'true' ? "text-white" : ""} uppercase font-opensans font-bold`}>Modules</p>
+    <div>
+      <button 
+        onMouseOver={() => setHover({ button: cookies.aeye_authenticated === "true", dropdown: hover.dropdown })}
+        onMouseOut={() => setHover({ button: false, dropdown: hover.dropdown })}
+        onClick={() => {
+            if (cookies.aeye_authenticated !== 'true') {
+                const authenticated = prompt('Enter The Modules Password') === 'Sandbox';
+                if (authenticated) {
+                  setCookies('aeye_authenticated', authenticated, {path: '/', expires: new Date(Date.now() + 10519200000)});
+                  alert('You Now Have Access to The \'Modules\' Dropdown!');
+                } else {
+                  alert('That Password Is Incorrect');
+                }
+            }
+        }}
+      >
+        <p className={`text-xs ${cookies.aeye_authenticated === 'true' ? "text-white" : ""} uppercase font-opensans font-bold`}>Modules</p>
       </button>
-      <div className={`absolute ${!(hover.button || hover.dropdown) && "hidden"} -mt-1 -ml-10 justify-start rounded-b divide-y divide-moduleDarkBlue bg-navbar`}
-              onMouseOver={() => setHover({ button: hover.button, dropdown: cookies.isAuthenticated === "true"})}
-              onMouseOut={() => setHover({ button: hover.button, dropdown: false})}>
-          <br />
-          {
-              descriptions.modules.map((module) =>
-                  <div className="py-1 mx-2 text-left whitespace-pre" key={module.dropdownTitle}>
-                      <a className="text-xs uppercase font-opensans font-bold text-white hover:text-moduleTeal" href={`/modules/${module.path}`} key={module.number.toString()}>
-                          {(module.parentModule ? "\t" : "") + module.dropdownTitle}
-                      </a>
-                  </div>
-              )
-          }
+      <div 
+        className={`absolute ${!(hover.button || hover.dropdown) && "hidden"} -mt-1 -ml-10 justify-start rounded-b divide-y divide-moduleDarkBlue bg-navbar`}
+        onMouseOver={() => setHover({ button: hover.button, dropdown: cookies.aeye_authenticated === "true"})}
+        onMouseOut={() => setHover({ button: hover.button, dropdown: false})}
+      >
+        <br />
+        {
+          descriptions.modules.map((module) =>
+              <div className="py-1 mx-2 text-left whitespace-pre" key={module.dropdownTitle}>
+                  <a className="text-xs uppercase font-opensans font-bold text-white hover:text-moduleTeal" href={`/modules/${module.path}`} key={module.number.toString()}>
+                      {(module.parentModule ? "\t" : "") + module.dropdownTitle}
+                  </a>
+              </div>
+          )
+        }
       </div>
     </div>
   );
